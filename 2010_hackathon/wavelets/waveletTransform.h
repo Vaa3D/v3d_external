8b772d543cc040c7b79431aec929069fea1ad3f2
Nicolas Chenouard <nicolas.chenouard@epfl.ch>
2010-08-04 18:52:02 +0000

    optimized wavelet 2D

--- 2010_hackathon/wavelets/waveletTransform.h
+++ 2010_hackathon/wavelets/waveletTransform.h
@@ -9,6 +9,7 @@
 
 double** b3WaveletScales(double* dataIn, int szx, int szy, int szZ, int numScales)  throw (WaveletConfigException);
 double** b3WaveletScales2D(double* dataIn, int width, int height, int numScales) throw (WaveletConfigException);
+double** b3WaveletScales2DOptimized(double* dataIn, int width, int height, int numScales) throw (WaveletConfigException);
 double** b3WaveletScalesOptimized(double* dataIn, int szx, int szy, int szZ, int numScales)  throw (WaveletConfigException);
 double** b3WaveletScalesOptimized2(double* dataIn, int szx, int szy, int szZ, int numScales)  throw (WaveletConfigException);
 void b3WaveletCoefficientsInplace(double** coefficients, double* originalImage, double* lowPass, int numScales, int numVoxels);
