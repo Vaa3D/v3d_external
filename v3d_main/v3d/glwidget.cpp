a89c461bf2b32679c557c872b429f97e1a7a8890
Luis Ibanez <luis.ibanez@kitware.com>
2010-08-11 00:49:43 +0000

    The method GLWidget::readAnoFile(QString file_anoText) was not defined in the header file, and it doesn't compile either. We now place it inside #if 0 to disable it. It probably should be removed.

--- v3d_main/v3d/glwidget.cpp
+++ v3d_main/v3d/glwidget.cpp
@@ -96,7 +96,9 @@ GLWidget::~GLWidget()
 
 }
 
-void GLWidget::readAnoFile(QString file_anoText)
+// FIXME :  This function doesn't compile. Should we remove it ?
+#if 0
+bool GLWidget::readAnoFile(QString file_anoText)
 {
 	printf("anofile=[%s]\n", qPrintable(file_anoText));
 	
@@ -122,6 +124,7 @@ void GLWidget::readAnoFile(QString file_anoText)
 	else
 		return true;
 }
+#endif
 
 void GLWidget::setXRotation(int angle)
 {
