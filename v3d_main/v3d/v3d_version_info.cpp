030e2db4058eed2b16ce6f9d6394dfecee828361
Hanchuan Peng <hanchuan.peng@gmail.com>
2010-08-23 14:38:25 +0000

    v2.520 with the new plugin-call-plugin interface

--- v3d_main/v3d/v3d_version_info.cpp
+++ v3d_main/v3d/v3d_version_info.cpp
@@ -38,7 +38,7 @@ Peng, H, Ruan, Z., Atasoy, D., and Sternson, S. (2010) “Automatic reconstructi
 
 #include <QtGui>
 
-QString versionnumber = "V3D (2.519), V3D-Neuron (2.0), V3D Plugin Interface (2.0) ";
+QString versionnumber = "V3D (2.520), V3D-Neuron (2.0), V3D Plugin Interface (2.1) ";
 
 void v3d_aboutinfo()
 {
