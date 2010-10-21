96958de3a8f2fff9c37c821ecfdba109f97e7c75
Hanchuan Peng <hanchuan.peng@gmail.com>
2010-10-21 13:45:33 +0000

    v2.536 fix a minor issue of xformwidget key_v press for uint8

--- v3d_main/v3d/v3d_core.cpp
+++ v3d_main/v3d/v3d_core.cpp
@@ -3189,11 +3189,11 @@ void XFormWidget::keyPressEvent(QKeyEvent * e)
 
 #if COMPILE_TARGET_LEVEL != 0
 		case Qt::Key_V:
-			if(imgData->getDatatype()!=V3D_UINT8) //only work for UINT8 data
-			{
-				v3d_msg("Your data type is not UINT8 yet, - you will need to convert to UINT8 to see the data in 3D. Go to main menu \"Image/Data\" -> \"Image type\" to convert.");
-				break;
-			}
+//			if(imgData->getDatatype()!=V3D_UINT8) //only work for UINT8 data
+//			{
+//				v3d_msg("Your data type is not UINT8 yet, - you will need to convert to UINT8 to see the data in 3D. Go to main menu \"Image/Data\" -> \"Image type\" to convert.");
+//				break;
+//			}
 
 		    if (QApplication::keyboardModifiers()==Qt::ControlModifier) //launch the full-image 3d view
 		    {
