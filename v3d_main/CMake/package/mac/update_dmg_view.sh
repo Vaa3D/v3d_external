e44ca55d78e15f9dcd78a86b8e0b43d09548330f
Christopher Bruns <brunsc@janelia.hhmi.org>
2010-11-22 21:29:52 +0000

    Added parsing of v3d version within update_dmg_view.sh script for creating .DS_Store file for installer. Updated dmg_DS_Store for current version number of v3d (2.542).

--- v3d_main/CMake/package/mac/update_dmg_view.sh
+++ v3d_main/CMake/package/mac/update_dmg_view.sh
@@ -9,18 +9,21 @@ rm -f temprw.dmg
 # Create temporary writeable DMG so we can manipulate its DS_Store
 hdiutil convert "$1" -format UDRW -o temprw.dmg
 
+v3d_version=`echo $1 | cut -f2 -d '-' | cut -f1,2 -d '.'`
+echo ${v3d_version}
+
 # Mount new DMG and remember the device name.
 device=$(hdiutil attach -readwrite -noverify -noautoopen temprw.dmg | egrep '^/dev/' | sed 1q | awk '{print $1}')
 sleep 2
 
-mkdir /Volumes/v3d-2.539/.background
+mkdir /Volumes/v3d-${v3d_version}/.background
 # background file must not be hidden when setting background
-chflags nohidden /Volumes/v3d-2.539/background.png
+chflags nohidden /Volumes/v3d-${v3d_version}/background.png
 
 # Use applescript language to arrange view
-echo '
-   tell application "Finder"
-     tell disk "v3d-2.539"
+echo "
+   tell application \"Finder\"
+     tell disk \"v3d-${v3d_version}\"
            open
            set current view of container window to icon view
            set toolbar visible of container window to false
@@ -30,17 +33,20 @@ echo '
            set arrangement of theViewOptions to not arranged
            set icon size of theViewOptions to 96
            set text size of theViewOptions to 16
-           set bgFile to file ":background.png"
+           set bgFile to file \":background.png\"
            set background picture of theViewOptions to bgFile
-           set position of item "v3d.app" of container window to {80, 110}
-           set position of item "Applications" of container window to {350, 110}
+           set position of item \"v3d.app\" of container window to {80, 110}
+           set position of item \"Applications\" of container window to {350, 110}
            update without registering applications
            delay 2
      end tell
    end tell
-' | osascript
+" | osascript
 
 # Rehide the background image
-chflags hidden /Volumes/v3d-2.539/background.png
+chflags hidden /Volumes/v3d-${v3d_version}/background.png
 sleep 2
+
+cp "/Volumes/v3d-${v3d_version}/.DS_Store" "./DS_Store_v3d_${v3d_version}"
+hdiutil detach "/Volumes/v3d-${v3d_version}"
 
