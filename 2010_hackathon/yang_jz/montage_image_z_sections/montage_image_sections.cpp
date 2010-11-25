bd60d3cc35ae29296f1b30409dc599bbc3767cf9
yangj13 <yangj13@janelia.hhmi.org>
2010-11-25 22:20:34 +0000

    check in the first montage z sections plugin

---
+++ 2010_hackathon/yang_jz/montage_image_z_sections/montage_image_sections.cpp
@@ -0,0 +1,205 @@
+/*
+ *  data_combination.cpp
+ *  data_combination
+ *
+ *  Created by Yang, Jinzhu and Hanchuan Peng, on 11/22/10.
+ *
+ */
+
+#include "data_combination.h"
+#include "v3d_message.h"
+
+//Q_EXPORT_PLUGIN2 ( PluginName, ClassName )
+//The value of PluginName should correspond to the TARGET specified in the plugin's project file.
+Q_EXPORT_PLUGIN2(threshold, ThPlugin);
+
+template <class T> 
+void DataCombination(T *apsInput, T * aspOutput, V3DLONG iImageWidth, V3DLONG iImageHeight, V3DLONG iImageLayer, V3DLONG h, V3DLONG d)
+{
+	V3DLONG i, j,k,n,count;
+	double t, temp;
+	
+	V3DLONG mCount = iImageHeight * iImageWidth;
+	V3DLONG mCount1 = iImageWidth*iImageLayer;
+	///////////////////////////data combination
+	//iImageLayer = 50;
+	if (iImageLayer<= iImageWidth)
+	{
+		for (i=0; i<iImageLayer; i++)
+		{
+			for (j=0; j<iImageHeight; j++)
+			{
+				for (k=0; k<iImageWidth; k++)
+				{
+					aspOutput[j* mCount1 + k  + (i * iImageWidth) ]= apsInput[i * mCount + j*iImageWidth + k];
+										
+				}
+			}
+		}			
+		
+	}
+		//////////////						
+}
+
+void thimg(V3DPluginCallback &callback, QWidget *parent, int method_code);
+
+//plugin funcs
+const QString title = "data combination threshold transform";
+QStringList ThPlugin::menulist() const
+{
+    return QStringList() 
+	<< tr("data_combination");
+	//<< tr("3D (set parameters)")
+	//<< tr("Help");
+}
+
+void ThPlugin::domenu(const QString &menu_name, V3DPluginCallback &callback, QWidget *parent)
+{
+	if (menu_name == tr("data_combination"))
+	{
+    	thimg(callback, parent,1 );
+    }
+	else if (menu_name == tr("Help"))
+	{
+		     
+		//QMessageBox::information(parent, "Version info, data combinationa1.0 (2010-11-24): this plugin is developed by Jinzhu Yang");		
+		//QMessageBox::information(parent," The adaptive segmentation function, each pixel threshold is statistical, method is to calculated average each piont of three-dimensional  6 neighborhood ");
+		//v3d_msg("Fail to allocate memory in Distance Transform./n ,fda ");		
+		return;
+	}
+
+}
+
+void thimg(V3DPluginCallback &callback, QWidget *parent, int method_code)
+{
+	v3dhandle curwin = callback.currentImageWindow();
+	V3DLONG h;
+	V3DLONG d;
+	if (!curwin)
+	{
+		v3d_msg("You don't have any image open in the main window.");
+		return;
+	}
+	int start_t = clock(); // record time point
+	
+	Image4DSimple* subject = callback.getImage(curwin);
+	QString m_InputFileName = callback.getImageName(curwin);
+	
+	if (!subject)
+	{
+		QMessageBox::information(0, title, QObject::tr("No image is open."));
+		return;
+	}	
+	Image4DProxy<Image4DSimple> pSub(subject);
+	
+	V3DLONG sz0 = subject->getXDim();
+    V3DLONG sz1 = subject->getYDim();
+    V3DLONG sz2 = subject->getZDim();
+	V3DLONG sz3 = subject->getCDim();
+	V3DLONG pagesz_sub = sz0*sz1*sz2;
+	
+	//----------------------------------------------------------------------------------------------------------------------------------
+	V3DLONG channelsz = sz0*sz1*sz2;
+	void *pData=NULL;
+	
+	V3DLONG sz_data[4]; sz_data[0]=sz0; sz_data[1]=sz1; sz_data[2]=sz2; sz_data[3]=1;
+		switch (subject->getDatatype()) 
+		{
+			case V3D_UINT8:
+				
+				try
+				{
+					pData = (void *)(new unsigned char [sz3*channelsz]); 
+				}
+					catch (...)
+				{
+					v3d_msg("Fail to allocate memory in data combination.");
+					if (pData) {delete []pData; pData=0;}
+					return;
+				}
+				
+				{
+					unsigned char * pSubtmp_uint8 = pSub.begin();
+				
+					for (V3DLONG ich=0; ich<sz3; ich++)
+						DataCombination(pSubtmp_uint8+ich*channelsz, (unsigned char *)pData+ich*channelsz, sz0, sz1, sz2, h, d  );
+				}
+				
+				break;
+				
+			case V3D_UINT16:
+				
+				try
+				{
+					pData = (void *)(new short int [sz3*channelsz]); 
+				}
+					catch (...)
+				{
+					v3d_msg("Fail to allocate memory in data combination.");
+					if (pData) {delete []pData; pData=0;}
+					return;
+				}
+				
+				{
+					short int * pSubtmp_uint16 = (short int *)pSub.begin();
+				
+					for (V3DLONG ich=0; ich<sz3; ich++)
+						DataCombination(pSubtmp_uint16+ich*channelsz, (short int *)pData+ich*channelsz, sz0, sz1, sz2, h, d );
+				}
+				
+				break;
+			case V3D_FLOAT32:
+				
+				try
+				{
+					pData = (void *)(new float [sz3*channelsz]); 
+				}
+					catch (...)
+				{
+					v3d_msg("Fail to allocate memory in data combination.");
+					if (pData) {delete []pData; pData=0;}
+					return;
+				}
+				
+				{
+					float * pSubtmp_float32 = (float *)pSub.begin();
+					
+					for (V3DLONG ich=0; ich<sz3; ich++)
+						DataCombination(pSubtmp_float32+ich*channelsz, (float *)pData+ich*channelsz, sz0, sz1, sz2, h, d );
+				}
+				
+				break;
+			default:
+				break;
+		}
+	
+	//----------------------------------------------------------------------------------------------------------------------------------
+	
+	int end_t = clock();
+	printf("time eclapse %d s for dist computing!\n", (end_t-start_t)/1000000);
+	
+	Image4DSimple p4DImage;
+	p4DImage.setData((unsigned char*)pData, sz0*sz2, sz1, 1, sz3, subject->getDatatype());
+	
+	v3dhandle newwin;
+	if(QMessageBox::Yes == QMessageBox::question (0, "", QString("Do you want to use the existing window?"), QMessageBox::Yes, QMessageBox::No))
+		newwin = callback.currentImageWindow();
+	else
+		newwin = callback.newImageWindow();
+	
+	callback.setImage(newwin, &p4DImage);
+	callback.setImageName(newwin, QString("data combination"));
+	callback.updateImageWindow(newwin);
+}
+
+
+void AdaTDialog::update()
+{
+	//get current data
+	
+	Dn = Dnumber->text().toLong()-1;
+	Dh = Ddistance->text().toLong()-1;
+
+		//printf("channel %ld val %d x %ld y %ld z %ld ind %ld \n", c, data1d[ind], nx, ny, nz, ind);
+	
+}
---
+++ 2010_hackathon/yang_jz/montage_image_z_sections/montage_image_sections.h
@@ -0,0 +1,134 @@
+/*
+ *  data_combination.h
+ *  data_combination
+ *
+ *  Created by Yang, Jinzhu on 11/22/10.
+ *  
+ *
+ */
+
+#ifndef __DISTTRANFORM_H__DataCombination
+#define __DISTTRANFORM_H__
+
+#include <QtGui>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "v3d_interface.h"
+
+class ThPlugin : public QObject, public V3DPluginInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(V3DPluginInterface);
+	
+public:
+	QStringList menulist() const;
+	void domenu(const QString &menu_name, V3DPluginCallback &callback, QWidget *parent);
+	
+	QStringList funclist() const {return QStringList();}
+	void dofunc(const QString &func_name, const V3DPluginArgList &input, V3DPluginArgList &output, QWidget *parent) {}
+	
+	template <class T> 
+	void DataCombination(T *apsInput, T * aspOutput, V3DLONG iImageWidth, V3DLONG iImageHeight, V3DLONG iImageLayer, V3DLONG h, V3DLONG d){}
+	//void DataCombination(float *apsInput, V3DLONG iImageWidth, V3DLONG iImageHeight, V3DLONG iImageLayer, V3DLONG* label){}
+	//void DataCombination(unsigned char *apsInput, unsigned char * aspOutput, V3DLONG iImageWidth, V3DLONG iImageHeight, V3DLONG iImageLayer, V3DLONG h){}
+};
+
+//define a simple dialog for choose DT parameters
+class AdaTDialog : public QDialog
+{
+	Q_OBJECT
+	
+public:
+	int ch;
+	bool b_use_1stmarker; //whether or not use the intensity value at the first marker location as the target value
+	bool b_rescale; //if rescale the value of the output
+	
+	QGridLayout *gridLayout;
+	QLabel* label_imagename;
+	QLabel* label_channel;
+	QComboBox* combo_channel;
+	QCheckBox* check_marker;
+	QCheckBox* check_rescale;
+
+	
+	QLabel *labelx;
+	QLabel *labely;
+	QLabel *labelz;
+	QLabel *labelz1;
+	QLabel *labelz2;	
+	QSpinBox** nval;
+	QLabel** nlabel;
+	
+	QSpinBox* Ddistance;
+	QSpinBox* Dnumber;
+	
+	QPushButton* ok;
+	QPushButton* cancel;
+	
+	V3DLONG Dn;
+	V3DLONG Dh;
+	
+	V3DLONG pagesz;
+	V3DLONG sc;
+	
+	///
+public:
+	AdaTDialog(V3DPluginCallback &cb, QWidget *parent)
+	{
+		Image4DSimple* image = cb.getImage(cb.currentImageWindow());
+		QString imageName = cb.getImageName(cb.currentImageWindow());		
+		sc = image->getCDim();
+		//create a dialog
+		Ddistance= new QSpinBox();
+		Dnumber = new QSpinBox();
+	
+		
+		Dnumber->setMaximum(255); Dnumber->setMinimum(1); Dnumber->setValue(3);
+		Ddistance ->setMaximum(255); Ddistance ->setMinimum(1); Ddistance ->setValue(6);
+	
+		
+		ok     = new QPushButton("OK");
+		cancel = new QPushButton("Cancel");
+		gridLayout = new QGridLayout();
+		
+		labelx = new QLabel(QObject::tr("sampling interval"));
+		labely = new QLabel(QObject::tr("number of sampling point"));
+       // labelz = new QLabel(QObject::tr("The adapitive threshold function."));	
+		//labelz1 = new QLabel(QObject::tr("Ddistance represents sampling interval"));			
+		//labelz2 = new QLabel(QObject::tr("Dnumber*Ddistance represents total of sampling point"));	
+		
+		
+		gridLayout->addWidget(labelx, 0,0); gridLayout->addWidget(Ddistance, 0,1);
+		gridLayout->addWidget(labely, 1,0); gridLayout->addWidget(Dnumber, 1,1);
+		
+		//gridLayout->addWidget(labelz, 2,0);
+		//gridLayout->addWidget(labelz1, 3,0);
+		//gridLayout->addWidget(labelz2, 4,0);
+		
+		
+		gridLayout->addWidget(cancel, 6,1); gridLayout->addWidget(ok, 6,0);
+		setLayout(gridLayout);
+		setWindowTitle(QString("Change parameters"));
+		
+		connect(ok,     SIGNAL(clicked()), this, SLOT(accept()));
+		connect(cancel, SIGNAL(clicked()), this, SLOT(reject()));
+		
+		//slot interface
+		connect(Ddistance, SIGNAL(valueChanged(int)), this, SLOT(update()));
+		connect(Dnumber,SIGNAL(valueChanged(int)), this, SLOT(update()));
+		
+	}
+
+	
+	~AdaTDialog(){}
+	
+	public slots:
+	void update();
+};
+
+
+
+#endif
+
+
