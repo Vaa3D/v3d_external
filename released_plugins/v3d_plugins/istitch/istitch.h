3f4b501a9a0966b5d7e16564ff8ed627b0c45c8d
Hanchuan Peng <hanchuan.peng@gmail.com>
2011-02-03 19:56:42 +0000

    check in the istitch (image stitching) plugin

---
+++ released_plugins/v3d_plugins/istitch/istitch.h
@@ -0,0 +1,44 @@
+/* istitch.h
+ * 2010-02-08: create this program by Yang Yu
+ */
+
+
+#ifndef __ISTITCH_H__
+#define __ISTITCH_H__
+
+//#define COMPILE_TO_COMMANDLINE 1
+
+//
+#include <QtGui>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <v3d_interface.h>
+
+//----------------------------------------------------------------------
+#ifdef COMPILE_TO_COMMANDLINE
+#else
+
+class IStitchPlugin : public QObject, public V3DPluginInterface2
+{
+    Q_OBJECT
+    Q_INTERFACES(V3DPluginInterface2);
+	
+public:
+	QStringList menulist() const;
+	void domenu(const QString &menu_name, V3DPluginCallback2 &callback, QWidget *parent);
+	
+	QStringList funclist() const {return QStringList();}
+	bool dofunc(const QString & func_name, const V3DPluginArgList & input, V3DPluginArgList & output,
+				V3DPluginCallback2 & v3d, QWidget * parent) {return true;}
+	float getPluginVersion() const {return 1.01f;} // version info 
+};
+
+#endif
+//----------------------------------------------------------------------
+
+#endif
+
+
+
---
+++ released_plugins/v3d_plugins/istitch/istitch.pro
@@ -0,0 +1,28 @@
+
+TEMPLATE      = lib
+CONFIG       += qt plugin warn_off
+#CONFIG       += x86_64
+
+V3DMAINDIR = ../../../v3d_external/v3d_main
+
+INCLUDEPATH  += $$V3DMAINDIR/basic_c_fun
+
+HEADERS       = istitch.h 
+HEADERS      +=	istitch_gui.h
+HEADERS      +=	$$V3DMAINDIR/basic_c_fun/stackutil.h
+HEADERS      +=	$$V3DMAINDIR/basic_c_fun/mg_utilities.h
+HEADERS      += $$V3DMAINDIR/basic_c_fun/mg_image_lib.h
+
+SOURCES       = istitch.cpp
+SOURCES      +=	$$V3DMAINDIR/basic_c_fun/stackutil.cpp
+SOURCES      +=	$$V3DMAINDIR/basic_c_fun/mg_utilities.cpp
+SOURCES      +=	$$V3DMAINDIR/basic_c_fun/mg_image_lib.cpp
+
+LIBS         += -lm -L$$V3DMAINDIR/common_lib/lib -lv3dtiff
+LIBS         += -lpthread
+LIBS	     += -lfftw3f -lfftw3f_threads
+#LIBS	     += -L/usr/local/lib/libfftw3f.a -L/usr/local/lib/libfftw3f_threads.a
+
+TARGET        = $$qtLibraryTarget(imageStitch)
+DESTDIR       = $$V3DMAINDIR/../v3d/plugins/iStitch
+
