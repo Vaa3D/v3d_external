20a470edfa660a4a8b8e9b980ed9a59dff7c5e7c
Hanchuan Peng <hanchuan.peng@gmail.com>
2011-02-05 02:01:26 +0000

    retrograde the montage image plugin to the last compilable state

--- released_plugins/v3d_plugins/montage_image_z_sections/montage_image_sections.cpp
+++ released_plugins/v3d_plugins/montage_image_z_sections/montage_image_sections.cpp
@@ -31,11 +31,9 @@ void montage_image_sections (T *apsInput, T * aspOutput, V3DLONG iImageWidth, V3
 			{
 				if (i < column) 
 				{
-					//aspOutput[(j+1)* column *iImageWidth + (k+1)  + (i * iImageWidth) ]= apsInput[i * mCount + j*iImageWidth + k];
-					aspOutput[(j)* column *iImageWidth + (k)  + (i * iImageWidth) ]= apsInput[i * mCount + j*iImageWidth + k];
+					aspOutput[j* column *iImageWidth + k  + (i * iImageWidth) ]= apsInput[i * mCount + j*iImageWidth + k];						
 				}else
 				{
-					//aspOutput[(((i/column)*iImageHeight)+ (j+1) )* column* iImageWidth + (k+1) + ((i%column) * iImageWidth)]= apsInput[i * mCount + j*iImageWidth + k];	
 					aspOutput[(((i/column)*iImageHeight)+ j )* column* iImageWidth + k + ((i%column) * iImageWidth)]= apsInput[i * mCount + j*iImageWidth + k];	
 				}
 			}
@@ -115,93 +113,82 @@ void do_computation(V3DPluginCallback2 &callback, QWidget *parent, int method_co
 	if (remainder == 0)
 	{
 		row = column;
-		channelsz1 = (sz0)*(sz1)*sz2;
+		channelsz1 = sz0*sz1*sz2;
 	}else 
 	{
 		row = column+1;
-		channelsz1 = (sz0)*(sz1)*column*(column+1);
+		channelsz1 = sz0*sz1*column*(column+1);
 	}	
 	channelsz = sz0*sz1*sz2;
   //  printf("column=%d,remainder=%d,row=%d sz3=%d\n",column,remainder,row,sz3);
 	void *pData=NULL;
 	V3DLONG sz_data[4]; sz_data[0]=sz0; sz_data[1]=sz1; sz_data[2]=sz2; sz_data[3]=1;
-	switch (subject->getDatatype()) 
-	{
-		case V3D_UINT8:
-			
-			try
-			{
-				pData = (void *)(new unsigned char [sz3*channelsz1]()); 
-			}
-				catch (...)
-			{
-				v3d_msg("Fail to allocate memory in data combination.");
-				if (pData) {delete []pData; pData=0;}
-				return;
-			}
-			
-			{
-				unsigned char * pSubtmp_uint8 = pSub.begin();
-			
-				for (V3DLONG ich=0; ich<sz3; ich++)
-					montage_image_sections(pSubtmp_uint8+ich*channelsz, (unsigned char *)pData+ich*channelsz1, sz0, sz1, sz2);
-			}
-			break;
-			
-		case V3D_UINT16:
-			try
-			{
-				pData = (void *)(new short int [sz3*channelsz1]()); 
-			}
-				catch (...)
-			{
-				v3d_msg("Fail to allocate memory in data combination.");
-				if (pData) {delete []pData; pData=0;}
-				return;
-			}
-			
-			{
-				short int * pSubtmp_uint16 = (short int *)pSub.begin();
-			
-				for (V3DLONG ich=0; ich<sz3; ich++)
-					montage_image_sections(pSubtmp_uint16+ich*channelsz, (short int *)pData+ich*channelsz1, sz0, sz1, sz2 );
-			}
-			break;
-		case V3D_FLOAT32:
-			
-			try
-			{
-				pData = (void *)(new float [sz3*channelsz1]()); 
-			}
-				catch (...)
-			{
-				v3d_msg("Fail to allocate memory in data combination.");
-				if (pData) {delete []pData; pData=0;}
-				return;
-			}
-			
-			{
-				float * pSubtmp_float32 = (float *)pSub.begin();
-				
-				for (V3DLONG ich=0; ich<sz3; ich++)
-					montage_image_sections(pSubtmp_float32+ich*channelsz, (float *)pData+ich*channelsz, sz0, sz1, sz2 );
-			}
-			
-			break;
-		default:
-			break;
-	}
-	
-	//V3DLONG width= column*sz0;
-	//V3DLONG Height = row*sz1;
-	
-	for (V3DLONG j=0; j<row; j++)
-	{
-		for(V3DLONG k=0; k<column; k++)
+		switch (subject->getDatatype()) 
 		{
-			(unsigned char*)pData[j*column+k] = 255;	
+			case V3D_UINT8:
+				
+				try
+				{
+					pData = (void *)(new unsigned char [sz3*channelsz1]()); 
+				}
+					catch (...)
+				{
+					v3d_msg("Fail to allocate memory in data combination.");
+					if (pData) {delete []pData; pData=0;}
+					return;
+				}
+				
+				{
+					unsigned char * pSubtmp_uint8 = pSub.begin();
+				
+					for (V3DLONG ich=0; ich<sz3; ich++)
+						montage_image_sections(pSubtmp_uint8+ich*channelsz, (unsigned char *)pData+ich*channelsz1, sz0, sz1, sz2);
+				}
+				break;
+				
+			case V3D_UINT16:
+				try
+				{
+					pData = (void *)(new short int [sz3*channelsz1]()); 
+				}
+					catch (...)
+				{
+					v3d_msg("Fail to allocate memory in data combination.");
+					if (pData) {delete []pData; pData=0;}
+					return;
+				}
+				
+				{
+					short int * pSubtmp_uint16 = (short int *)pSub.begin();
+				
+					for (V3DLONG ich=0; ich<sz3; ich++)
+						montage_image_sections(pSubtmp_uint16+ich*channelsz, (short int *)pData+ich*channelsz1, sz0, sz1, sz2 );
+				}
+				break;
+			case V3D_FLOAT32:
+				
+				try
+				{
+					pData = (void *)(new float [sz3*channelsz1]()); 
+				}
+					catch (...)
+				{
+					v3d_msg("Fail to allocate memory in data combination.");
+					if (pData) {delete []pData; pData=0;}
+					return;
+				}
+				
+				{
+					float * pSubtmp_float32 = (float *)pSub.begin();
+					
+					for (V3DLONG ich=0; ich<sz3; ich++)
+						montage_image_sections(pSubtmp_float32+ich*channelsz, (float *)pData+ich*channelsz, sz0, sz1, sz2 );
+				}
+				
+				break;
+			default:
+				break;
 		}
-	}
 	
 	//----------------------------------------------------------------------------------------------------------------------------------
 	
@@ -210,10 +197,7 @@ void do_computation(V3DPluginCallback2 &callback, QWidget *parent, int method_co
 	///v3d_msg("2");
 	//printf("column=%d,remainder=%d,row=%d\n",column,remainder,row);
 	Image4DSimple p4DImage;
-	
 	p4DImage.setData((unsigned char*)pData, (V3DLONG)column*sz0, (V3DLONG)row*sz1, 1, sz3, subject->getDatatype());	
-	
-	//p4DImage.setData((unsigned char*)pData, (V3DLONG)column*(sz0), (V3DLONG)row*(sz1), 1, sz3, subject->getDatatype());
 	//	printf("sz0= %d sz1=%d sz2=%d dd=%d vv=%d\n", sz0,sz1,sz2,dd,vv);
 	v3dhandle newwin;
 	if(QMessageBox::Yes == QMessageBox::question (0, "", QString("Do you want to use the existing window?"), QMessageBox::Yes, QMessageBox::No))
@@ -222,6 +206,6 @@ void do_computation(V3DPluginCallback2 &callback, QWidget *parent, int method_co
 		newwin = callback.newImageWindow();
 	
 	callback.setImage(newwin, &p4DImage);
-	callback.setImageName(newwin, QString("montage_image_sections"));
+	callback.setImageName(newwin, QString("thresholded image"));
 	callback.updateImageWindow(newwin);
 }
