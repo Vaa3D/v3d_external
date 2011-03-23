2ed46ad4d46da3316d687c7d82dc61ed4d789761
Hanchuan Peng <hanchuan.peng@gmail.com>
2011-03-23 15:19:44 +0000

    change matlab IO toolbox -ltiff to -lv3dtiff

--- matlab_io_basicdatatype/makeosmex_rawfile_io.m
+++ matlab_io_basicdatatype/makeosmex_rawfile_io.m
@@ -3,10 +3,11 @@
 %%
 %%by Hanchuan Peng
 %% 2007-03-06
-% last update: 090722: by Hanchuan Peng. This makes it easire to compile all mex files under this directory
+% last update: 090722: by Hanchuan Peng. This makes it easier to compile all mex files under this directory
+%% 2011-03-23: change to -lv3dtiff
 
-mex loadRaw2Stack_c.cpp mg_image_lib.cpp mg_utilities.cpp -ltiff
-mex saveStack2File_c.cpp mg_image_lib.cpp mg_utilities.cpp -ltiff
+mex loadRaw2Stack_c.cpp mg_image_lib.cpp mg_utilities.cpp -L../v3d_main/common_lib/lib -lv3dtiff
+mex saveStack2File_c.cpp mg_image_lib.cpp mg_utilities.cpp -L../v3d_main/common_lib/lib -lv3dtiff
 
 mex checkMachineEndian.cpp
 
