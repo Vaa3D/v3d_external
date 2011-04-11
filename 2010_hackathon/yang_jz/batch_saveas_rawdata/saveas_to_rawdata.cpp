c04b481bdd6bd41c7cd4381f84e91824657270dd
yangj13 <yangj13@janelia.hhmi.org>
2011-04-11 15:37:02 +0000

    check in batch saves tif to raw

---
+++ 2010_hackathon/yang_jz/batch_saveas_rawdata/saveas_to_rawdata.cpp
@@ -0,0 +1,268 @@
+/*
+ *  saveas_to_rawdata .cpp
+ *  saveas_to_rawdata.h 
+ *
+ *  Created by Yang, Jinzhu, on 04/08/11.
+ *  
+ */
+
+#include <QtGlobal>
+#include <dirent.h>
+#include "saveas_to_rawdata.h"
+#include "v3d_message.h" 
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include <vector>
+#include <list>
+#include <bitset>
+
+#include <set>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include <cmath>
+
+#include <ctime>
+
+#include <complex>
+#include <fftw3.h>
+
+#include "basic_surf_objs.h"
+#include "stackutil.h"
+#include "volimg_proc.h"
+#include "img_definition.h"
+#include "basic_landmark.h"
+
+#include "mg_utilities.h"
+#include "mg_image_lib.h"
+#include "basic_4dimage.h"
+
+#include "imageio_mylib.h" 
+
+// interface v3d
+#include <QtGui>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <v3d_interface.h>
+
+#include <QBasicTimer>
+#include <QPolygonF>
+//#include <QMainWindow>
+#include <qgroupbox.h>
+
+#include "volimg_proc.h"
+#include <iostream>
+
+#include <QMutex>
+#include <QQueue>
+
+//Q_EXPORT_PLUGIN2 ( PluginName, ClassName )
+//The value of PluginName should correspond to the TARGET specified in the plugin's project file.
+Q_EXPORT_PLUGIN2(saveas_to_rawdata, SAVEAS_TO_RAWDATAlugin);
+
+
+//plugin funcs
+const QString title = "saveas_to_rawdata";
+QStringList SAVEAS_TO_RAWDATAlugin::menulist() const
+{
+    return QStringList() 
+	<<tr("saveas_to_rawdata")
+	<<tr("Help");
+}
+
+void SAVEAS_TO_RAWDATAlugin::domenu(const QString &menu_name, V3DPluginCallback &callback, QWidget *parent)
+{
+	if (menu_name == tr("saveas_to_rawdata"))
+	{
+    	saveas_to_rawdata(callback, parent,1 );
+    }
+	else if (menu_name == tr("Help"))
+	{
+		v3d_msg("(version 0.11) Convert a SWC file to a mask image, where the area of the swc tubes will have non-zero values, and other area will have 0s; Use mask imge to filter tif image, where the area of the swc non-zero will tif image values not change, and other area will have 0");
+		return;
+	}
+}
+
+void saveas_to_rawdata(V3DPluginCallback &callback, QWidget *parent, int method_code)
+{
+		
+	QString fn_img = QFileDialog::getExistingDirectory(0, QObject::tr("Choose the directory including all tiled images "),
+											   QDir::currentPath(),
+											   QFileDialog::ShowDirsOnly);
+	QDir dir(fn_img);
+
+	//QString dirname = dir.dirName();
+	
+	//dir.mkdir(dirname);
+	
+	QStringList list = dir.entryList();
+	
+	QStringList rootlist;
+	
+	//qDebug()<<"fn_img dirname"<<fn_img<<dirname;
+		
+	foreach (QString subDir,dir.entryList(QDir::Dirs|QDir::NoDotAndDotDot))
+	{
+		rootlist += QFileInfo(dir, subDir).absoluteFilePath();
+	}
+    
+	for(int i=0; i<rootlist.size();i++)
+	{
+		
+		QString str = QFileInfo(rootlist.at(i)).fileName(); 
+		str+=".raw";
+		dir.mkdir(str);
+	}
+	
+	for(int i=0; i<rootlist.size();i++)
+	{
+		
+		QStringList myList;
+		
+		myList.clear();
+	
+		// get the image files namelist in the directory
+		QStringList imgSuffix;
+		imgSuffix<<"*.tif"<<"*.raw"<<"*.lsm"
+		<<"*.TIF"<<"*.RAW"<<"*.LSM";
+		
+		QDir dir1(rootlist.at(i));
+		
+	//	QString dirname = dir1.dirName();  
+//		
+//		dirname+="1";
+//		
+//		dir1.mkdir(dirname);
+		
+		if (!dir1.exists())
+		{
+			qWarning("Cannot find the directory");
+			return ;
+		}
+		
+		foreach (QString file, dir1.entryList(imgSuffix, QDir::Files, QDir::Name))
+		{
+			myList += QFileInfo(dir1, file).absoluteFilePath();
+		}
+		
+		qDebug()<<"lisitsize== "<<myList.size();
+		
+		//qDebug()<<"dirname= "<<rootlist.at(i);
+		
+		foreach (QString qs, myList) qDebug() << qs;
+		
+	    for(int j=0; j<myList.size();j++)
+		{	
+			//char * curFileSuffix = getSurfix(const_cast<char *>(myList.at(j).c_str()));		
+		
+			QString curFilePath = QFileInfo(myList.at(j)).path();
+			
+			QString name = QFileInfo(myList.at(j)).fileName(); 
+			
+		//	curFilePath.append("/");
+			
+			void *pData = NULL;
+			
+			ImagePixelType datatype;
+			
+			string fn_sub;
+						
+			QString curPath = curFilePath;
+			
+			qDebug() << "image filel: name: "<< myList.at(j)<<name;
+			
+			//qDebug() << "suffix ... " << curFileSuffix; // 
+			
+			qDebug() << "curPath ... " << curPath ; // 
+			
+			curPath = curPath+".raw"+"/";
+			
+			qDebug() << "curPath ===== " << curPath ; // 
+			
+			string fn = myList.at(j).toStdString();
+//			
+//			string ff = myList.at(j).toStdString();
+			
+	//		string fn = name.toStdString();
+			
+			string ff = name.toStdString();
+			
+			qDebug()<<"fn="<<fn.c_str();
+			
+			int i = ff.find(".", 0 ); 
+			
+			fn_sub=ff.substr(0,i);
+			
+			char * imgSrcFile = const_cast<char *>(fn.c_str());
+			
+			qDebug()<<"fn_sub="<<fn_sub.c_str();
+			
+			//printf("i=%ld\n",i);
+			
+			V3DLONG *sz = 0; 
+			
+			int datatype_relative = 0;
+			
+			unsigned char* tmpData = 0;
+			
+			int pixelnbits=1; //100817		
+			
+			if(loadTif2StackMylib(imgSrcFile,tmpData,sz,datatype_relative,pixelnbits)!=true)
+			//if(loadImage(imgSrcFile,tmpData,sz,datatype_relative)!=true)
+			{
+				QMessageBox::information(0, "Load Image", QObject::tr("File load failure"));
+				return;
+			}
+			if(datatype_relative ==1)
+			{
+				datatype = V3D_UINT8;
+			}
+			else if(datatype_relative == 2)
+			{
+				datatype = V3D_UINT16;
+			}
+			else if(datatype_relative==4)
+			{
+				datatype = V3D_FLOAT32;
+			}
+			
+			QString tmp_filename = curPath;
+			
+			tmp_filename.append(QString(fn_sub.c_str()));
+			
+			tmp_filename += ".raw";	
+			
+			qDebug()<<"tmp_filename=="<<tmp_filename;
+			
+		//	sz_tmp[0] = vx; sz_tmp[1] = vy; sz_tmp[2] = vz; sz_tmp[3] = vc; 
+			
+			if (saveImage(tmp_filename.toStdString().c_str(), (const unsigned char *)tmpData, sz, datatype)!=true)
+			{
+				fprintf(stderr, "Error happens in file writing. Exit. \n");
+				return ;
+			}
+			
+			
+		}
+	}
+	
+}
+void SetsizeDialog::update()
+{
+	//get current data
+	
+	NX = coord_x->text().toLong()-1;
+	NY = coord_y->text().toLong()-1;
+	NZ = coord_z->text().toLong()-1;
+
+	
+	
+}
