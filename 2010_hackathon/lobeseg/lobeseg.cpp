f31dbad92f8786d24649dba4ab8031506cc8b627
Hanchuan Peng <hanchuan.peng@gmail.com>
2011-05-25 17:51:12 +0000

    a declearation of the function of one-sode lobe seg

--- 2010_hackathon/lobeseg/lobeseg.cpp
+++ 2010_hackathon/lobeseg/lobeseg.cpp
@@ -296,5 +296,9 @@ bool do_lobeseg_bdbminus(unsigned char *inimg1d, const V3DLONG sz[4], unsigned c
 }
 
 
+bool do_lobeseg_bdbminus_onesideonly(unsigned char *inimg1d, const V3DLONG sz[4], unsigned char *outimg1d, int in_channel_no, int out_channel_no, const BDB_Minus_ConfigParameter & mypara)
+{
+	return true;
+}
 
 
