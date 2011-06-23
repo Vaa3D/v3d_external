b2fbdc4decbafa73237d92a3a7518eed0953c2fa
Hanchuan Peng <hanchuan.peng@gmail.com>
2011-06-23 19:13:40 +0000

    v2.654 add a new intensity rescaling function in V3D convert menu

--- v3d_main/v3d/v3d_version_info.cpp
+++ v3d_main/v3d/v3d_version_info.cpp
@@ -49,7 +49,7 @@ Peng, H, Ruan, Z., Atasoy, D., and Sternson, S. (2010) “Automatic reconstructi
 
 namespace v3d {
     // Set current version here.
-    VersionInfo thisVersionOfV3D(2.653);
+    VersionInfo thisVersionOfV3D(2.654);
 
     QString versionnumber = "V3D (" +
         thisVersionOfV3D.toQString() +
