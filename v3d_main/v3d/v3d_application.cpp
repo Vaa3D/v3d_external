d409c59b121dcd7ea3c95c4b3c4ef2b7d6ed9a5f
Hanchuan Peng <hanchuan.peng@gmail.com>
2011-07-06 00:17:24 +0000

    v2.657. force to initialize the mainwindow and namainwindow pointers to be 0. seems fix the strange Mac Leopard 10.5 crashing bug. need further test.

--- v3d_main/v3d/v3d_application.cpp
+++ v3d_main/v3d/v3d_application.cpp
@@ -1,14 +1,16 @@
 #include "v3d_application.h"
 
-MainWindow* V3dApplication::mainWindow;
-NaMainWindow* V3dApplication::naMainWindow;
+MainWindow* V3dApplication::mainWindow=0; //reset to be 0. by Hanchuan Peng, 20110705
+NaMainWindow* V3dApplication::naMainWindow=0; //reset to be 0. by Hanchuan Peng, 20110705
 bool V3dApplication::mainWindowIsActive;
 bool V3dApplication::naMainWindowIsActive;
 
 V3dApplication::V3dApplication(int & argc, char ** argv) :
     QApplication(argc, argv)
 {
-    V3dApplication::mainWindow = new MainWindow();
+	if (!V3dApplication::mainWindow) //force checking. by Hanchuan Peng, 20110705
+		V3dApplication::mainWindow = new MainWindow();
+
     V3dApplication::mainWindowIsActive = false;
     V3dApplication::naMainWindowIsActive = false;
 }
