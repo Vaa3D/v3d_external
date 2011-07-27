de2f7d17c63d5e4a9179f7a79339872efd46be5c
xiaoh10 <xiaoh10@janelia.hhmi.org>
2011-07-27 20:29:22 +0000

    add marker to feature function

--- 2010_hackathon/hang/partial_align/main/feature_compare.h
+++
@@ -1,3 +0,0 @@
-#ifdef __FEATURE_COMPARE_H_
-#define __FEATURE_COMPARE_H_
-#endif
--- 2010_hackathon/hang/partial_align/main/main.cpp
+++ 2010_hackathon/hang/partial_align/main/main.cpp
@@ -8,37 +8,55 @@
 #include <string>
 
 #include "stackutil.h"
-#include "mark_detector.h"
+#include "basic_types.h"
+#include "utils.h"
 
 using namespace std;
 
+void printHelp();
+
 int main(int argc, char* argv[])
 {
-	if(argc == 1)
+	if(argc <= 3)
 	{
-		cout<<"Usage : partial_align [option] input_img marker/feature"<<endl;
-		cout<<""<<endl;
-		cout<<"-mark-detector     in_img out_marker"<<endl;
-		cout<<"-calc-feature      in_img [in_marker] out_feature "<<endl;
-		cout<<"-feature-comprare  img1 feature1 img2 feature2"<<endl;
-		cout<<""<<endl;
+		printHelp();
 		return 0;
 	}
+
 	if(string(argv[1]) == "-mark-detector")
 	{
-		if(argc!=4){cout<<"usage: parlia_align -mark-detector in_img out_marker"<<endl; return 0;}
+		if(argc!=4){printHelp(); return 0;}
 
 		unsigned char * data1d = 0;
 		V3DLONG *sz = 0;
 		int datatype;
 		loadImage(argv[2], data1d, sz, datatype);
 
-		list<MyImageMarker> markerlist;
-		detect_mark(markerlist, data1d, sz);
-		writeMarker_file(argv[3], markerlist);
+		vector<MarkerType> vecMarker;
+		detect_marker(vecMarker, data1d, sz);
+		writeMarker_file(argv[3], vecMarker);
 		if(data1d){delete [] data1d; data1d = 0;}
 	}
 	if(string(argv[2]) == "-calc-feature")
 	{
+		unsigned char * data1d = 0;
+		V3DLONG *sz = 0;
+		int datatype;
+		loadImage(argv[2], data1d, sz, datatype);
+
+		vector<MarkerType> vecMarker;
+		if(argc==4) detect_marker(vecMarker, data1d, sz);
+		else if(argc==5) vecMarker = readMarker_file(argv[3]);
+		else {printHelp(); return 0;}
 	}
+}
+
+void printHelp()
+{
+		cout<<"Usage : partial_align [option] input_img marker/feature"<<endl;
+		cout<<""<<endl;
+		cout<<"-mark-detector     in_img out_marker"<<endl;
+		cout<<"-calc-feature      in_img [in_marker] out_feature "<<endl;
+		cout<<"-feature-comprare  img1 feature1 img2 feature2"<<endl;
+		cout<<""<<endl;
 }
