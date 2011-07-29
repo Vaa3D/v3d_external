f7264181b60649c58adb87e0780736e07c65d10d
Hanchuan Peng <hanchuan.peng@gmail.com>
2011-07-29 00:16:15 +0000

    v2.671 enhanced color management interface

--- v3d_main/v3d/v3d_version_info.cpp
+++ v3d_main/v3d/v3d_version_info.cpp
@@ -49,7 +49,7 @@ Peng, H, Ruan, Z., Atasoy, D., and Sternson, S. (2010) “Automatic reconstructi
 
 namespace v3d {
     // Set current version here.
-    VersionInfo thisVersionOfV3D(2.670);
+    VersionInfo thisVersionOfV3D(2.671);
 
     QString versionnumber = "V3D (" +
         thisVersionOfV3D.toQString() +
