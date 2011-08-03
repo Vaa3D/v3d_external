1790bc2b81028e91425cda447f956ce2be738579
yuy <yuy@janelia.hhmi.org>
2011-08-03 15:11:17 +0000

    update image blend by using mutual information to detect reference channel

--- 2010_hackathon/yuy/imageblend/imageblend.cpp
+++ 2010_hackathon/yuy/imageblend/imageblend.cpp
@@ -34,6 +34,114 @@ Q_EXPORT_PLUGIN2(imageBlend, ImageBlendPlugin);
 // func
 int image_blending(V3DPluginCallback2 &callback, QWidget *parent);
 
+// func mutual information for pair images with the same size
+template <class Tdata>
+double mi_computing(Tdata *pImg1, Tdata *pImg2, V3DLONG szImg, int datatype)
+{
+    size_t start_t = clock();
+    
+    // joint histogram
+    double **jointHistogram = NULL;
+    double *img1Hist=NULL;
+    double *img2Hist=NULL;
+    
+    V3DLONG szHist;
+    
+    if(datatype==1) // 8-bit UINT8
+    {
+        szHist = 256;
+    }
+    else if(datatype==2) // 12-bit UINT16
+    {
+        szHist = 4096;
+    }
+    else
+    {
+        std::cout<<"Datatype is not supported!"<<endl;
+        return -1;
+    }
+    
+    V3DLONG denomHist = szHist*szHist;
+    
+    try
+    {
+        jointHistogram = new double * [szHist];
+        for(int i=0; i<szHist; i++)
+        {
+            jointHistogram[i] = new double [szHist];
+            memset(jointHistogram[i], 0, sizeof(double)*szHist);
+        }
+        
+        img1Hist = new double [szHist];
+        img2Hist = new double [szHist];
+        
+        memset(img1Hist, 0, sizeof(double)*szHist);
+        memset(img2Hist, 0, sizeof(double)*szHist);
+    }
+    catch(...)
+    {
+        qDebug()<<"Fail to allocate memory for joint histogram!";
+        return -1;
+    }
+    
+    //
+    for(V3DLONG i=0; i<szImg; i++)
+    {
+        jointHistogram[ (V3DLONG)pImg1[i] ][ (V3DLONG)pImg2[i] ] ++;  
+    }
+    
+    double jointEntropy=0, img1Entropy=0, img2Entropy=0;
+    
+    // normalized joint histogram
+    for(V3DLONG i=0; i<szHist; i++)
+    {
+        for(V3DLONG j=0; j<szHist; j++)
+        {
+            jointHistogram[i][j] /= (double)(denomHist);
+            
+            double val = jointHistogram[i][j]?jointHistogram[i][j]:1;
+            
+            jointEntropy += val * log2(val);
+        }
+    }
+
+    // marginal histogram
+    for(V3DLONG i=0; i<szHist; i++)
+    {
+        for(V3DLONG j=0; j<szHist; j++)
+        {
+            img1Hist[i] += jointHistogram[i][j];
+            img2Hist[i] += jointHistogram[j][i];
+        } 
+    }
+    
+    for(V3DLONG i=0; i<szHist; i++)
+    {
+        double val1 = img1Hist[i]?img1Hist[i]:1;
+        double val2 = img2Hist[i]?img2Hist[i]:1;
+        
+        img1Entropy += val1 * log2(val1);
+        img2Entropy += val2 * log2(val2);
+    }
+    
+    // de-alloc
+    if(jointHistogram) {
+        for(int i=0; i<szHist; i++)
+        {
+            delete[] jointHistogram[i];
+        }     
+        delete []jointHistogram; jointHistogram=NULL;
+    }
+    if(img1Hist) {delete []img1Hist; img1Hist=NULL;}
+    if(img2Hist) {delete []img2Hist; img2Hist=NULL;}
+    
+    qDebug() << "time elapse for computing mutual information ... " <<clock()-start_t;
+    
+    // MI
+    return (jointEntropy - img1Entropy - img2Entropy);    
+    
+}
+
 //plugin funcs
 const QString title = "Image Blending";
 QStringList ImageBlendPlugin::menulist() const
@@ -58,7 +166,7 @@ void ImageBlendPlugin::domenu(const QString &menu_name, V3DPluginCallback2 &call
 // function call
 QStringList ImageBlendPlugin::funclist() const
 {
-	return QStringList() << "ImageBlend";
+	return QStringList() << "imageBlend";
 }
 
 bool ImageBlendPlugin::dofunc(const QString & func_name, const V3DPluginArgList & input, V3DPluginArgList & output, V3DPluginCallback2 & v3d, QWidget * parent)
@@ -71,18 +179,13 @@ bool ImageBlendPlugin::dofunc(const QString & func_name, const V3DPluginArgList 
 // stitching 2 images and display in V3D
 int image_blending(V3DPluginCallback2 &callback, QWidget *parent)
 {
-    qDebug()<<"open dialog ...";
     
     ImageBlendingDialog dialog(callback, parent, NULL);
 	if (dialog.exec()!=QDialog::Accepted)
 		return -1;
-    
-    qDebug()<<"dialog opened ...";
 
     QString m_InputFileName1 = dialog.fn_img1;
     QString m_InputFileName2 = dialog.fn_img2;
-    
-    qDebug()<<"got file names ...";
     
     // load images
     V3DLONG *sz_img1 = 0; 
@@ -118,14 +221,17 @@ int image_blending(V3DPluginCallback2 &callback, QWidget *parent)
         return -4;
     }
     
-    // find reference : suppose reference color channel with most information
-    V3DLONG sum1 = 0, sum2 = 0;
-    V3DLONG ref1, ref2, nullcolor;
-    bool b_existNULL=false, b_img1NULL;
+    //
+    V3DLONG pagesz = sz_img1[0]*sz_img1[1]*sz_img1[2];
     
-    for(V3DLONG c=0; c<sz_img1[3]; c++)
+    // find reference : suppose reference color channels similar enough
+    V3DLONG ref1=0, ref2=0, nullcolor1 = -1, nullcolor2 = -1;
+    bool b_img1existNULL=false, b_img2existNULL=false;
+    
+    // step 1: find null color channel
+    for(V3DLONG c=0; c<sz_img1[3]; c++) // image 1
     {
-        V3DLONG offset_c = c*sz_img1[0]*sz_img1[1]*sz_img1[2];
+        V3DLONG offset_c = c*pagesz;
         V3DLONG sumint1 = 0;
         for (V3DLONG k=0; k<sz_img1[2]; k++) 
         {
@@ -137,29 +243,38 @@ int image_blending(V3DPluginCallback2 &callback, QWidget *parent)
                 {
                     V3DLONG idx = offset_j + i;
                     
-                    sumint1 += p1dImg1[idx];
+                    if(datatype_img1 == V3D_UINT8)
+                    {
+                        sumint1 += p1dImg1[idx];
+                    }
+                    else if(datatype_img1 == V3D_UINT16)
+                    {
+                        sumint1 += ((unsigned short *)p1dImg1)[idx];
+                    }
+                    else if(datatype_img1 == V3D_FLOAT32)
+                    {
+                        sumint1 += ((float *)p1dImg1)[idx];
+                    }
+                    else
+                    {
+                    
+                    }
                 }
             }
         }
         
+        qDebug()<<"sum ..."<<sumint1<<c;
+        
         if(sumint1<EMPTY)
         {
-            b_existNULL = true;
-            b_img1NULL = false;
-            nullcolor = c;
-        }
-        
-        if(sumint1>sum1)
-        {
-            sum1 = sumint1;
-            ref1 = c;
-        }
-        
+            b_img1existNULL = true;
+            nullcolor1 = c;
+        }        
     }
     
-    for(V3DLONG c=0; c<sz_img2[3]; c++)
+    for(V3DLONG c=0; c<sz_img2[3]; c++) // image 2
     {
-        V3DLONG offset_c = c*sz_img1[0]*sz_img1[1]*sz_img1[2];
+        V3DLONG offset_c = c*pagesz;
         V3DLONG sumint2 = 0;
         for (V3DLONG k=0; k<sz_img1[2]; k++) 
         {
@@ -171,35 +286,108 @@ int image_blending(V3DPluginCallback2 &callback, QWidget *parent)
                 {
                     V3DLONG idx = offset_j + i;
                     
-                    sumint2 += p1dImg2[idx];
+                    if(datatype_img1 == V3D_UINT8)
+                    {
+                        sumint2 += p1dImg2[idx];
+                    }
+                    else if(datatype_img1 == V3D_UINT16)
+                    {
+                        sumint2 += ((unsigned short *)p1dImg2)[idx];
+                    }
+                    else if(datatype_img1 == V3D_FLOAT32)
+                    {
+                        sumint2 += ((float *)p1dImg2)[idx];
+                    }
+                    else
+                    {
+                        
+                    }
                 }
             }
         }
         
+        qDebug()<<"sum ..."<<sumint2<<c;
+        
         if(sumint2<EMPTY)
         {
-            b_existNULL = true;
-            b_img1NULL = true;
-            nullcolor = c;
-        }
-        
-        if(sumint2>sum2)
-        {
-            sum2 = sumint2;
-            ref2 = c;
+            b_img2existNULL = true;
+            nullcolor2 = c;
         }
     }
     
-    qDebug()<<"ref ..."<<ref1<<ref2;
+    // step 2: find ref color channel by compute MI
+    double scoreMI = -1e10; // -INF
+    for(V3DLONG c1=0; c1<sz_img1[3]; c1++)
+    {
+        if(b_img1existNULL)
+        {
+            if(c1==nullcolor1) continue;
+        }
+        
+        for(V3DLONG c2=0; c2<sz_img2[3]; c2++)
+        {
+            if(b_img2existNULL)
+            {
+                if(c2==nullcolor2) continue;
+            }
+            
+            if(datatype_img1 == V3D_UINT8)
+            {
+                unsigned char* pImg1Proxy = p1dImg1 + c1*pagesz;
+                unsigned char* pImg2Proxy = p1dImg2 + c2*pagesz;
+                
+                double valMI = mi_computing<unsigned char>(pImg1Proxy, pImg2Proxy, pagesz, 1);
+                
+                if(valMI>scoreMI)
+                {
+                    scoreMI = valMI;
+                    
+                    ref1 = c1;
+                    ref2 = c2;
+                }
+            }
+            else if(datatype_img1 == V3D_UINT16)
+            {
+                unsigned short* pImg1Proxy = ((unsigned short *)p1dImg1) + c1*pagesz;
+                unsigned short* pImg2Proxy = ((unsigned short *)p1dImg2) + c2*pagesz;
+                
+                double valMI = mi_computing<unsigned short>(pImg1Proxy, pImg2Proxy, pagesz, 2);
+                
+                qDebug()<<"mi ..."<<valMI<<c1<<c2;
+                
+                if(valMI>scoreMI)
+                {
+                    scoreMI = valMI;
+                    
+                    ref1 = c1;
+                    ref2 = c2;
+                }
+            }
+            else if(datatype_img1 == V3D_FLOAT32)
+            {
+                printf("Currently this program dose not support FLOAT32.\n"); // temporary
+                return -1;
+            }
+            else
+            {
+                printf("Currently this program only support UINT8, UINT16, and FLOAT32 datatype.\n");
+                return -1;
+            }
+            
+        }
+    }
+    
+    qDebug()<<"ref ..."<<ref1<<ref2<<"null color ..."<<b_img1existNULL<<nullcolor1<<b_img2existNULL<<nullcolor2;
     
     // image blending	
     // suppose image1 and image2 have a common reference
     // the blended image color dim = image1 color dim + image2 color dim - 1
-    V3DLONG pagesz = sz_img1[0]*sz_img1[1]*sz_img1[2];
     V3DLONG colordim = sz_img1[3]+sz_img2[3]-1;
-    V3DLONG totalplxs = colordim * pagesz;
     
-    if(b_existNULL) colordim--;
+    if(b_img1existNULL) colordim--;
+    if(b_img2existNULL) colordim--;
+    
+    V3DLONG totalplxs = colordim * pagesz;
     
 	if(datatype_img1 == V3D_UINT8)
 	{
@@ -230,9 +418,9 @@ int image_blending(V3DPluginCallback2 &callback, QWidget *parent)
                 
                 V3DLONG c1 = c;
                 
-                if(b_existNULL && b_img1NULL)
+                if(b_img1existNULL)
                 {
-                    if(c1!=nullcolor)
+                    if(c1!=nullcolor1)
                     {
                         offset_c1 = c1*pagesz;
                     }
@@ -270,11 +458,13 @@ int image_blending(V3DPluginCallback2 &callback, QWidget *parent)
             if(!b_img1)
             {
                 
-                V3DLONG c2 = c - sz_img1[3] - 1;
+                V3DLONG c2 = c - (sz_img1[3] - 1);
                 
-                if(b_existNULL && !b_img1NULL)
+                if(b_img1existNULL) c2++;
+                
+                if(b_img2existNULL)
                 {
-                    if(c2!=nullcolor)
+                    if(c2!=nullcolor2)
                     {
                         offset_c2 = c2*pagesz;
                     }
@@ -383,9 +573,9 @@ int image_blending(V3DPluginCallback2 &callback, QWidget *parent)
                 
                 V3DLONG c1 = c;
                 
-                if(b_existNULL && b_img1NULL)
+                if(b_img1existNULL)
                 {
-                    if(c1!=nullcolor)
+                    if(c1!=nullcolor1)
                     {
                         offset_c1 = c1*pagesz;
                     }
@@ -423,11 +613,13 @@ int image_blending(V3DPluginCallback2 &callback, QWidget *parent)
             if(!b_img1)
             {
                 
-                V3DLONG c2 = c - sz_img1[3] - 1;
+                V3DLONG c2 = c - (sz_img1[3] - 1);
                 
-                if(b_existNULL && !b_img1NULL)
+                if(b_img1existNULL) c2++;
+                
+                if(b_img2existNULL)
                 {
-                    if(c2!=nullcolor)
+                    if(c2!=nullcolor2)
                     {
                         offset_c2 = c2*pagesz;
                     }
@@ -535,9 +727,9 @@ int image_blending(V3DPluginCallback2 &callback, QWidget *parent)
                 
                 V3DLONG c1 = c;
                 
-                if(b_existNULL && b_img1NULL)
+                if(b_img1existNULL)
                 {
-                    if(c1!=nullcolor)
+                    if(c1!=nullcolor1)
                     {
                         offset_c1 = c1*pagesz;
                     }
@@ -575,11 +767,13 @@ int image_blending(V3DPluginCallback2 &callback, QWidget *parent)
             if(!b_img1)
             {
                 
-                V3DLONG c2 = c - sz_img1[3] - 1;
+                V3DLONG c2 = c - (sz_img1[3] - 1);
                 
-                if(b_existNULL && !b_img1NULL)
+                if(b_img1existNULL) c2++;
+                
+                if(b_img2existNULL)
                 {
-                    if(c2!=nullcolor)
+                    if(c2!=nullcolor2)
                     {
                         offset_c2 = c2*pagesz;
                     }
@@ -663,7 +857,12 @@ int image_blending(V3DPluginCallback2 &callback, QWidget *parent)
 		printf("Currently this program only support UINT8, UINT16, and FLOAT32 datatype.\n");
 		return -1;
 	}
+    
+    // de-alloc
+    if(p1dImg1) {delete []p1dImg1; p1dImg1=NULL;}
+    if(p1dImg2) {delete []p1dImg2; p1dImg2=NULL;}
 	
+    //
 	return 0;
 	
 }
