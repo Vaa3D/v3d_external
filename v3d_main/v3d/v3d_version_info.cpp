d9640358600faee6199e96e6b6c67a8272a476ff
Hanchuan Peng <hanchuan.peng@gmail.com>
2011-08-09 03:17:15 +0000

    v2.686 add the user-customizable toolbar

--- v3d_main/v3d/v3d_version_info.cpp
+++ v3d_main/v3d/v3d_version_info.cpp
@@ -49,7 +49,7 @@ Peng, H, Ruan, Z., Atasoy, D., and Sternson, S. (2010) “Automatic reconstructi
 
 namespace v3d {
     // Set current version here.
-    VersionInfo thisVersionOfV3D(2.684);
+    VersionInfo thisVersionOfV3D(2.686);
 
     QString versionnumber = "V3D (" +
         thisVersionOfV3D.toQString() +
