609dc0a4f5088def8a1724497fb8a0fe4a52914f
xiaoh10 <xiaoh10@janelia.hhmi.org>
2011-08-23 21:29:52 +0000

    distance transform works but bad result

--- 2010_hackathon/hang/v3d_convert/dist_transform.h
+++ 2010_hackathon/hang/v3d_convert/dist_transform.h
@@ -17,7 +17,7 @@ template<class T> bool normal_distance_transform(double * &pDist, T* inimg1d, V3
 
 	for(V3DLONG i = 0; i < tol_sz; i++) pData[i] = (inimg1d[i] >= thresh) ? 1 : 0;
 
-	dt3d_binary(pData, pDist, pLabel, sz, 1);
+	dt3d_binary(pData, pDist, pLabel, sz, 0);
 
 	if(pLabel){delete [] pLabel; pLabel = 0;}
 	if(pData) {delete [] pData; pData = 0;}
