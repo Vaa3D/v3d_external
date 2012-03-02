b2b218edc47ad42dcd083b3c7717e8fa920ac7e6
Hanchuan Peng <hanchuan.peng@gmail.com>
2012-03-02 18:41:51 +0000

    v2.719 the scalebar in tri-view looks better

--- v3d_main/v3d/v3d_version_info.cpp
+++ v3d_main/v3d/v3d_version_info.cpp
@@ -49,7 +49,7 @@ Peng, H, Ruan, Z., Atasoy, D., and Sternson, S. (2010) “Automatic reconstructi
 
 namespace v3d {
     // Set current version here.
-    VersionInfo thisVersionOfV3D(2.718);
+    VersionInfo thisVersionOfV3D(2.719);
 
     QString versionnumber = "Vaa3D (3D Visualization-Assisted Analysis) (" +
         thisVersionOfV3D.toQString() +
