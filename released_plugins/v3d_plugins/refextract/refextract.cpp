2914e15b4ea4c63769d90b15979145d5a07a317e
yuy <yuy@janelia.hhmi.org>
2012-03-23 15:08:59 +0000

    update refextract codes by adding a template image generating function

--- released_plugins/v3d_plugins/refextract/refextract.cpp
+++ released_plugins/v3d_plugins/refextract/refextract.cpp
@@ -43,6 +43,35 @@ void freeMemory(Tdata *&p)
     if(p) {delete[] p; p=NULL;}
 }
 
+// Open a series of inputs
+QStringList importSeriesFileList_addnumbersort(const QString & curFilePath)
+{
+    QStringList myList;
+    myList.clear();
+
+    // get the image files namelist in the directory
+    QStringList imgSuffix;
+    imgSuffix<<"*.tif"<<"*.raw"<<"*.v3draw"<<"*.lsm"
+            <<"*.TIF"<<"*.RAW"<<"*.V3DRAW"<<"*.LSM";
+
+    QDir dir(curFilePath);
+    if (!dir.exists())
+    {
+        qWarning("Cannot find the directory");
+        return myList;
+    }
+
+    foreach (QString file, dir.entryList(imgSuffix, QDir::Files, QDir::Name))
+    {
+        myList += QFileInfo(dir, file).absoluteFilePath();
+    }
+
+    // print filenames
+    foreach (QString qs, myList)  qDebug() << qs;
+
+    return myList;
+}
+
 // extract reference channel and convert it to 8bit data
 template<class Tdata, class Tidx>
 bool extconv(Tdata *p, Tidx sx, Tidx sy, Tidx sz, unsigned char *&pOutput)
@@ -133,7 +162,8 @@ void errorPrint()
 QStringList RefExtractPlugin::funclist() const
 {
     return QStringList() << tr("refExtract")
-                         << tr("ref2mip");
+                         << tr("ref2mip")
+                         << tr("createTemplate");
 }
 
 bool RefExtractPlugin::dofunc(const QString & func_name, const V3DPluginArgList & input, V3DPluginArgList & output, V3DPluginCallback2 & callback,  QWidget * parent)
@@ -527,9 +557,9 @@ bool RefExtractPlugin::dofunc(const QString & func_name, const V3DPluginArgList 
                 int val = 0;
                 for(V3DLONG k=0; k<sz_relative[2]; k++)
                 {
-                   int curval = pOutput[ k*sz_relative[0]*sz_relative[1] + offset_j + i ];
+                    int curval = pOutput[ k*sz_relative[0]*sz_relative[1] + offset_j + i ];
 
-		   if(val < curval) val = curval;
+                    if(val < curval) val = curval;
                 }
 
                 pOutput[offset_j + i] = val;
@@ -543,6 +573,220 @@ bool RefExtractPlugin::dofunc(const QString & func_name, const V3DPluginArgList 
             sz_relative[2]=1;
             sz_relative[3]=1;
             if(!saveImage(qPrintable(qs_filename_img_output),pOutput,sz_relative,V3D_UINT8))
+            {
+                printf("ERROR: saveImage() return false!\n");
+                return false;
+            }
+        }
+
+        //
+        if(pOutput) {delete []pOutput; pOutput=NULL;}
+
+        //
+        return true;
+    }
+    else if (func_name == tr("createTemplate"))
+    {
+        if(input.size()<1 || (input.size()==1 && output.size()<1) ) // no inputs
+        {
+            //print Help info
+            errorPrint();
+            return true;
+        }
+
+        vector<char*> * infilelist;
+        vector<char*> * paralist;
+        vector<char*> * outfilelist;
+
+        char * infile = NULL; //input_image_folder
+        char * paras = NULL; // parameters
+        char * outfile = NULL; // output_image_file
+
+        if(input.size()>0) {infilelist = (vector<char*> *)(input.at(0).p);}
+        if(output.size()>0) { outfilelist = (vector<char*> *)(output.at(0).p);}  // specify output
+        if(input.size()>1) { paralist = (vector<char*> *)(input.at(1).p); paras =  paralist->at(0);} // parameters
+        if(!infilelist->empty()) { infile = infilelist->at(0); }
+        if(!outfilelist->empty()) { outfile = outfilelist->at(0); }
+
+        // init
+        V3DLONG channel_ref = 0;
+
+        QStringList imgList = importSeriesFileList_addnumbersort(infile);
+        QString qs_filename_img_input = imgList.at(0);
+        QString qs_filename_img_output;
+
+        // parsing parameters
+        if(paras)
+        {
+            int argc = 0;
+            int len = strlen(paras);
+            int posb[1000];
+
+            for(int i = 0; i < len; i++)
+            {
+                if(i==0 && paras[i] != ' ' && paras[i] != '\t')
+                {
+                    posb[argc++] = i;
+                }
+                else if((paras[i-1] == ' ' || paras[i-1] == '\t') && (paras[i] != ' ' && paras[i] != '\t'))
+                {
+                    posb[argc++] = i;
+                }
+            }
+
+            char **argv = NULL;
+            try
+            {
+                argv =  new char* [argc];
+                for(int i = 0; i < argc; i++)
+                {
+                    argv[i] = paras + posb[i];
+                }
+            }
+            catch(...)
+            {
+                printf("\nError: fail to allocate memory!\n");
+                return false;
+            }
+
+            for(int i = 0; i < len; i++)
+            {
+                if(paras[i]==' ' || paras[i]=='\t')
+                    paras[i]='\0';
+            }
+
+            char* key;
+            for(int i=0; i<argc; i++)
+            {
+                if(i+1 != argc) // check that we haven't finished parsing yet
+                {
+                    key = argv[i];
+
+                    qDebug()<<">>key ..."<<key;
+
+                    if (*key == '#')
+                    {
+                        while(*++key)
+                        {
+                            if (!strcmp(key, "c"))
+                            {
+                                channel_ref = atoi( argv[i+1] ) - 1; // red 1 green 2 blue 3
+                                i++;
+                            }
+                            else
+                            {
+                                cout<<"parsing ..."<<key<<i<<"Unknown command. Type 'v3d -x plugin_name -f function_name' for usage"<<endl;
+                                return false;
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        cout<<"parsing ..."<<key<<i<<"Unknown command. Type 'v3d -x plugin_name -f function_name' for usage"<<endl;
+                        return false;
+                    }
+
+                }
+            }
+
+            QString qs_basename_input=QFileInfo(qs_filename_img_input).baseName();
+            QString qs_filename_output=QString(outfile);
+            QString qs_pathname_output=QFileInfo(qs_filename_output).path();
+
+            if(outfile)
+            {
+                qs_filename_img_output=qs_filename_output;
+            }
+            else
+            {
+                qs_filename_img_output=qs_pathname_output+"/"+qs_basename_input+"_template.v3draw";
+            }
+
+            // error check
+            if(qs_filename_img_input==NULL || qs_filename_img_output==NULL)
+            {
+                printf("\nERROR: invalid input file name (target or subject)!\n");
+                errorPrint();
+                return false;
+            }
+            if(channel_ref<0)
+            {
+                printf("\nERROR: invalid reference channel! Assume R(1)G(2)B(3) ...!\n");
+                errorPrint();
+                return false;
+            }
+        }
+
+        //
+        float *pOutput = NULL;
+        V3DLONG *szOutput = NULL;
+        V3DLONG cnt = 1;
+        V3DLONG totalplxs = 0;
+        foreach (QString img_str, imgList)
+        {
+            string fn_image = img_str.toStdString();
+
+            //loading files
+            V3DLONG *szImg = 0;
+            int datatypeImg = 0;
+            unsigned char* pImg1d = 0;
+
+            if(QFile(QString(fn_image.c_str())).exists())
+            {
+                if (loadImage(const_cast<char *>(fn_image.c_str()), pImg1d, szImg, datatypeImg)!=true)
+                {
+                    fprintf (stderr, "Error happens in reading the subject file [%s]. Exit. \n", fn_image.c_str());
+                    return false;
+                }
+            }
+            else
+            {
+                cout<<"The input file does not exist!"<<endl;
+                return false;
+            }
+
+            if(cnt==1)
+            {
+                // assume all images are with the same size
+                try
+                {
+                    szOutput = new V3DLONG [4];
+                    szOutput[0] = szImg[0];
+                    szOutput[1] = szImg[1];
+                    szOutput[2] = szImg[2];
+                    szOutput[3] = szImg[3]; // 1
+
+                    totalplxs = szOutput[0]*szOutput[1]*szOutput[2]*szOutput[3];
+
+                    pOutput = new float [totalplxs];
+                    for(V3DLONG i=0; i<totalplxs; i++)
+                        pOutput[i] = pImg1d[i];
+
+                }
+                catch(...)
+                {
+                    cout<<"Fail to allocate memory!"<<endl;
+                    return false;
+                }
+            }
+            else
+            {
+                for(V3DLONG i=0; i<totalplxs; i++)
+                    pOutput[i] += pImg1d[i];
+            }
+
+            //
+            if(pImg1d) {delete []pImg1d; pImg1d=NULL;}
+            if(szImg) {delete []szImg; szImg=NULL;}
+
+            cnt++;
+        }
+
+        // save
+        if(qs_filename_img_output!=NULL)
+        {
+            if(!saveImage(qPrintable(qs_filename_img_output),(unsigned char *)pOutput,szOutput,4))
             {
                 printf("ERROR: saveImage() return false!\n");
                 return false;
