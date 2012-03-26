01c8a097c003f8682971459cd5cb1fa648d1bff9
rokickik <rokickik@janelia.hhmi.org>
2012-03-26 18:17:42 +0000

    added command-line support for V3DPBD

--- v3d_main/v3d/v3d_commandlineparser.cpp
+++ v3d_main/v3d/v3d_commandlineparser.cpp
@@ -102,6 +102,7 @@ bool CLP::check_filename(QString fn)
              (curfile_info.suffix().toUpper()=="RAW") ||
              (curfile_info.suffix().toUpper()=="RAW5") ||
              (curfile_info.suffix().toUpper()=="V3DRAW") ||
+             (curfile_info.suffix().toUpper()=="V3DPBD") ||
              (curfile_info.suffix().toUpper()=="IMG") ||
              (curfile_info.suffix().toUpper()=="HDR") ||
              (curfile_info.suffix().toUpper()=="NII") ||  
