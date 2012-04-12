881b2b4859ab06b7901d0b2d938b20d8cb5c0786
Hanchuan Peng <hanchuan.peng@gmail.com>
2012-04-12 02:15:13 +0000

    v2.741 now in the basic_4dimage.h, include two functions for createImage(), so to make it easier for plugins code to invoke these two functions, without the need to link to mylib

--- v3d_main/v3d/v3d_version_info.cpp
+++ v3d_main/v3d/v3d_version_info.cpp
@@ -49,7 +49,7 @@ Peng, H, Ruan, Z., Atasoy, D., and Sternson, S. (2010) “Automatic reconstructi
 
 namespace v3d {
     // Set current version here.
-    VersionInfo thisVersionOfV3D(2.740);
+    VersionInfo thisVersionOfV3D(2.741);
 
     QString versionnumber = "Vaa3D (3D Visualization-Assisted Analysis) (" +
         thisVersionOfV3D.toQString() +
