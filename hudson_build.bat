6bedfe8e429025afe71dcad437041435e84bafd0
fosterl <fosterljanelia.hhmi.org>
2012-11-27 21:26:10 +0000

    Added for hudson build

---
+++ hudson_build.bat
@@ -0,0 +1,13 @@
+:: 2012 Les Foster
+:: Borrowed from Hanshuan Peng's build.bat.
+:: For use in the Hudson build tool.
+::
+:: examples: 
+:: hudson_build.bat
+  
+cd v3d_main
+
+hudson_build.bat
+:: build of plugin-demo has been omitted in this version.
+
+
