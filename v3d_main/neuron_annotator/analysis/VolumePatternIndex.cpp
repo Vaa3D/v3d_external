33eed29c5b03f21197a8ab0bbd761e6c2c3224f7
murphys <murphys@janelia.hhmi.org>
2013-01-15 21:41:39 +0000

    Update.

--- v3d_main/neuron_annotator/analysis/VolumePatternIndex.cpp
+++ v3d_main/neuron_annotator/analysis/VolumePatternIndex.cpp
@@ -1,8 +1,73 @@
 
 /*
 
+  Index
+  ----------------------------------------------------------------------------
+
+  This utility indexes a set of 3-D images of 1-channel for search against a
+  query 3-D image also of 1-channel. The dimensions of the query and index
+  must match.
+
+  1. Number of Indices
+
+  We will start with an initial version having only a single index.
+
+  2. Index Characteristics
+
+  The index will consist of a reduced downsampling of the original image, the
+  magnitude of which is re-mapped according to the threshold set.
+
+  3. Bitwidth of Index
+
+  There are four thresholds used to partition the data: (with sample values)
+
+  0 - empty [0-5]
+  1 - low [6-19]
+  2 - medium [20-49]
+  3 - high [50 and up]
+
+  We will therefore use 2-bits to characterize a region within the index.
+
+  4. Size of Region
+
+  The region will downsample the original data by a factor of 1000, so that we
+  use 10x10x10 downsampling as default.
+
+  Search
+  -------------------------------------------------------------------------------
+
+  1. Steps
+
+  Search will be either one-phase or two-phase. If one-phase, or "fast", then
+  only the index will be used. If two-phase, then the results of the fast
+  search will be further refined in the second phase by using the actual source
+  image files.
+
+  A floating-point value will be multiplied by the "maxHits" parameters and this
+  number (rounded to the nearest integer) will be used as the number of fast
+  results to be forwarded to the second pass.
+
+  2. Scoring
+
+  There will be a 4x4 scoring matrix, used for both the fast phase and the second
+  (full) phase.
+
+  The default matrix will be:
 
 
+
+                              Subject
+
+                empty      low      medium      high
+
+
+     empty        0        -1        -2        -4
+
+Qry  low         -1         1         1         0
+
+     medium      -2         1         2         2
+
+     high        -4         0         2         4
 
 
  */
