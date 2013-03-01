ef16ea0fe895667075f77b409083e315f672afec
murphys <murphys@janelia.hhmi.org>
2013-03-01 19:26:14 +0000

    Added intensity normalization.

--- v3d_main/neuron_annotator/analysis/NeuronFragmentEditor.cpp
+++ v3d_main/neuron_annotator/analysis/NeuronFragmentEditor.cpp
@@ -66,110 +66,149 @@ bool NeuronFragmentEditor::execute()
 
 bool NeuronFragmentEditor::createFragmentComposite()
 {
-  // Open consolidated signal label file
-  ImageLoader sourceLoader;
-  My4DImage * sourceImage=sourceLoader.loadImage(sourceImageFilepath);
-  V3DLONG xdim=sourceImage->getXDim();
-  V3DLONG ydim=sourceImage->getYDim();
-  V3DLONG zdim=sourceImage->getZDim();
-  V3DLONG cdim=sourceImage->getCDim();
+    // Open consolidated signal label file
+    ImageLoader sourceLoader;
+    My4DImage * sourceImage=sourceLoader.loadImage(sourceImageFilepath);
+    V3DLONG xdim=sourceImage->getXDim();
+    V3DLONG ydim=sourceImage->getYDim();
+    V3DLONG zdim=sourceImage->getZDim();
+    V3DLONG cdim=sourceImage->getCDim();
 
-  qDebug() << "Using source x=" << xdim << " y=" << ydim << " z=" << zdim << " c=" << cdim;
+    qDebug() << "Using source x=" << xdim << " y=" << ydim << " z=" << zdim << " c=" << cdim;
 
-  ImageLoader labelLoader;
-  My4DImage * labelImage=labelLoader.loadImage(inputLabelIndexFilepath);
+    ImageLoader labelLoader;
+    My4DImage * labelImage=labelLoader.loadImage(inputLabelIndexFilepath);
 
-  qDebug() << "Checking source and label dimension correspondence";
+    qDebug() << "Checking source and label dimension correspondence";
 
-  if (labelImage->getXDim()!=xdim) {
-    qDebug() << "source and label xdim do not match";
-    return false;
-  }
-  if (labelImage->getYDim()!=ydim) {
-    qDebug() << "source and label ydim do not match";
-    return false;
-  }
-  if (labelImage->getZDim()!=zdim) {
-    qDebug() << "source and label zdim do not match";
-    return false;
-  }
-  if (cdim<3) {
-    qDebug() << "Expected source image to contain at least 3 channels";
-    return false;
-  }
-
-  qDebug() << "Creating compositeImage";
-
-  My4DImage * compositeImage = new My4DImage();
-  compositeImage->loadImage(xdim, ydim, zdim, cdim, V3D_UINT8);
-
-  qDebug() << "Setting up channel pointeres";
-
-  v3d_uint16 * label=(v3d_uint16*)(labelImage->getRawDataAtChannel(0));
-
-  v3d_uint8 * sourceR=sourceImage->getRawDataAtChannel(0);
-  v3d_uint8 * sourceG=sourceImage->getRawDataAtChannel(1);
-  v3d_uint8 * sourceB=sourceImage->getRawDataAtChannel(2);
-
-  v3d_uint8 * compR=compositeImage->getRawDataAtChannel(0);
-  v3d_uint8 * compG=compositeImage->getRawDataAtChannel(1);
-  v3d_uint8 * compB=compositeImage->getRawDataAtChannel(2);
-
-  qDebug() << "Creating fragment membership array";
-  
-  int fragmentListSize=fragmentList.size();
-  int * fragmentArr = new int[fragmentListSize];
-  for (int f=0;f<fragmentListSize;f++) {
-    fragmentArr[f]=fragmentList.at(f);
-    qDebug() << "Adding entry for fragment=" << fragmentArr[f];
-  }
-
-  qDebug() << "Populating new image";
-
-  for (V3DLONG z=0;z<zdim;z++) {
-    for (V3DLONG y=0;y<ydim;y++) {
-      for (V3DLONG x=0;x<xdim;x++) {
-	V3DLONG offset=z*ydim*xdim + y*xdim + x;
-	int labelValue=label[offset];
-	bool include=false;
-	for (int f=0;f<fragmentListSize;f++) {
-	  if (labelValue==fragmentArr[f]+1) {
-	    include=true;
-	  }
-	}
-	if (include) {
-	  compR[offset]=sourceR[offset];
-	  compG[offset]=sourceG[offset];
-	  compB[offset]=sourceB[offset];
-	} else {
-	  compR[offset]=0;
-	  compG[offset]=0;
-	  compB[offset]=0;
-	}
-      }
+    if (labelImage->getXDim()!=xdim) {
+        qDebug() << "source and label xdim do not match";
+        return false;
     }
-  }
+    if (labelImage->getYDim()!=ydim) {
+        qDebug() << "source and label ydim do not match";
+        return false;
+    }
+    if (labelImage->getZDim()!=zdim) {
+        qDebug() << "source and label zdim do not match";
+        return false;
+    }
+    if (cdim<3) {
+        qDebug() << "Expected source image to contain at least 3 channels";
+        return false;
+    }
 
-  qDebug() << "Saving composite";
+    qDebug() << "Creating compositeImage";
 
-  ImageLoader compositeLoader;
-  compositeLoader.saveImage(compositeImage, outputStackFilepath);
+    My4DImage * compositeImage = new My4DImage();
+    compositeImage->loadImage(xdim, ydim, zdim, cdim, V3D_UINT8);
 
-  qDebug() << "Creating mip";
+    qDebug() << "Setting up channel pointeres";
 
-  My4DImage * compositeMIP = createMIPFromImage(compositeImage);
+    v3d_uint16 * label=(v3d_uint16*)(labelImage->getRawDataAtChannel(0));
 
-  qDebug() << "Saving mip";
-  
-  ImageLoader compositeMIPSaver;
-  compositeMIPSaver.saveImage(compositeMIP, outputMipFilepath);
+    v3d_uint8 * sourceR=sourceImage->getRawDataAtChannel(0);
+    v3d_uint8 * sourceG=sourceImage->getRawDataAtChannel(1);
+    v3d_uint8 * sourceB=sourceImage->getRawDataAtChannel(2);
 
-  qDebug() << "Starting cleanup";
+    v3d_uint8 * compR=compositeImage->getRawDataAtChannel(0);
+    v3d_uint8 * compG=compositeImage->getRawDataAtChannel(1);
+    v3d_uint8 * compB=compositeImage->getRawDataAtChannel(2);
 
-  delete [] fragmentArr;
-  delete compositeImage;
-  delete compositeMIP;
-  return true;
+    qDebug() << "Creating fragment membership array";
+
+    int fragmentListSize=fragmentList.size();
+    int * fragmentArr = new int[fragmentListSize];
+    for (int f=0;f<fragmentListSize;f++) {
+        fragmentArr[f]=fragmentList.at(f);
+        qDebug() << "Adding entry for fragment=" << fragmentArr[f];
+    }
+
+    qDebug() << "Populating new image";
+
+    v3d_uint8 maxIntensity=0;
+
+    // First get max intensity
+    for (V3DLONG z=0;z<zdim;z++) {
+        for (V3DLONG y=0;y<ydim;y++) {
+            for (V3DLONG x=0;x<xdim;x++) {
+                V3DLONG offset=z*ydim*xdim + y*xdim + x;
+                int labelValue=label[offset];
+                bool include=false;
+                for (int f=0;f<fragmentListSize;f++) {
+                    if (labelValue==fragmentArr[f]+1) {
+                        include=true;
+                    }
+                }
+                if (include) {
+                    v3d_uint8 r=sourceR[offset];
+                    v3d_uint8 g=sourceG[offset];
+                    v3d_uint8 b=sourceB[offset];
+                    if (r>maxIntensity) {
+                        maxIntensity=r;
+                    }
+                    if (g>maxIntensity) {
+                        maxIntensity=g;
+                    }
+                    if (b>maxIntensity) {
+                        maxIntensity=b;
+                    }
+                }
+            }
+        }
+    }
+
+    for (V3DLONG z=0;z<zdim;z++) {
+        for (V3DLONG y=0;y<ydim;y++) {
+            for (V3DLONG x=0;x<xdim;x++) {
+                V3DLONG offset=z*ydim*xdim + y*xdim + x;
+                int labelValue=label[offset];
+                bool include=false;
+                for (int f=0;f<fragmentListSize;f++) {
+                    if (labelValue==fragmentArr[f]+1) {
+                        include=true;
+                    }
+                }
+                if (include) {
+                    v3d_uint8 r=sourceR[offset];
+                    v3d_uint8 g=sourceG[offset];
+                    v3d_uint8 b=sourceB[offset];
+                    double intensityFactor=254.0/(double)maxIntensity;
+                    r=((double)r*intensityFactor);
+                    g=((double)g*intensityFactor);
+                    b=((double)b*intensityFactor);
+                    compR[offset]=r;
+                    compG[offset]=g;
+                    compB[offset]=b;
+                } else {
+                    compR[offset]=0;
+                    compG[offset]=0;
+                    compB[offset]=0;
+                }
+            }
+        }
+    }
+
+    qDebug() << "Saving composite";
+
+    ImageLoader compositeLoader;
+    compositeLoader.saveImage(compositeImage, outputStackFilepath);
+
+    qDebug() << "Creating mip";
+
+    My4DImage * compositeMIP = createMIPFromImage(compositeImage);
+
+    qDebug() << "Saving mip";
+
+    ImageLoader compositeMIPSaver;
+    compositeMIPSaver.saveImage(compositeMIP, outputMipFilepath);
+
+    qDebug() << "Starting cleanup";
+
+    delete [] fragmentArr;
+    delete compositeImage;
+    delete compositeMIP;
+    return true;
 }
 
 My4DImage * NeuronFragmentEditor::createMIPFromImage(My4DImage * image) {
