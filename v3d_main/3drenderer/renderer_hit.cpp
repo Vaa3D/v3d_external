9c5ed5ba857897b627794431d111a574b8fa4080
Hanchuan Peng <hanchuan.peng@gmail.com>
2013-04-26 07:50:12 +0000

    it seems the ROI makes sense now

--- v3d_main/3drenderer/renderer_hit.cpp
+++ v3d_main/3drenderer/renderer_hit.cpp
@@ -4044,7 +4044,7 @@ int Renderer_gl1::zoomview_wheel_event()//by PHC, 20130424
     QDir pluginsDir1 = pluginsDir;
     if (pluginsDir1.cd("plugins/teramanager")==true)
     {
-        //b_grabhighrez = true;
+        b_grabhighrez = true;
         produceZoomViewOf3DRoi(loc_vec,
                                2    //2 means zoom-in from wheel event
                                );
