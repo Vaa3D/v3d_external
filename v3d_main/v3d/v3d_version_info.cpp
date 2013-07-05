386c2a17311d74d6307486a0f5ab2c66d82b8de9
Hanchuan Peng <hanchuan.peng@gmail.com>
2013-07-05 19:37:17 +0000

    slight change of the help info

--- v3d_main/v3d/v3d_version_info.cpp
+++ v3d_main/v3d/v3d_version_info.cpp
@@ -60,7 +60,7 @@ namespace v3d {
 void v3d_aboutinfo()
 {
     QString helptext =
-        "<H3>Vaa3D: A Swiss army knife for 3D/4D/5D volume image and surface visualization and processing, developed by Hanchuan Peng, Zongcai Ruan, Fuhui Long, et al. (JFRC, HHMI). All rights reserved.</H3> "
+        "<H3>Vaa3D: A Swiss army knife for 3D/4D/5D volume image and surface visualization and processing, developed by Hanchuan Peng, Zongcai Ruan, Fuhui Long, et al. All rights reserved.</H3> "
 //      "<H3><span style=\"color:#FF0000\">If you are seeing this red color information, you are using an alpha-testing version of V3D. If you experience any problem, please contact Hanchuan Peng. </span></H3> "
         "If you have used Vaa3D in your research and would like to cite it, please cite as the following: <br><br>"
         "<span style=\"color:#0000FF\">Peng, H., Ruan, Z., Long, F., Simpson, J.H., and Myers, E.W. (2010) \"V3D enables real-time 3D visualization and quantitative analysis of large-scale biological image data sets,\" Nature Biotechnology, Vol. 28, No. 4, pp.348-353, DOI: 10.1038/nbt.1612. (http://vaa3d.org) </span><br> "
