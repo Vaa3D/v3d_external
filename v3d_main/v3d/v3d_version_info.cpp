4b33dc0c28e02cd8fe96e5987f1ab741ea47a771
Hanchuan Peng <hanchuan.peng@gmail.com>
2013-11-06 18:58:47 +0000

    v2.856 fix a bug on not assign one return value for the newly revised IO wrapper when the one slice loading function has been added

--- v3d_main/v3d/v3d_version_info.cpp
+++ v3d_main/v3d/v3d_version_info.cpp
@@ -50,7 +50,7 @@ Peng, H, Ruan, Z., Atasoy, D., and Sternson, S. (2010) “Automatic reconstructi
 
 namespace v3d {
     // Set current version here.
-    VersionInfo thisVersionOfV3D(2.855);
+    VersionInfo thisVersionOfV3D(2.856);
 
     QString versionnumber = "Vaa3D (3D Visualization-Assisted Analysis) (" +
         thisVersionOfV3D.toQString() +
