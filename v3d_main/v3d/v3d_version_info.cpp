41f7326f201858859e9801426d97e24eedb906ad
Hanchuan Peng <hanchuan.peng@gmail.com>
2014-07-31 22:55:01 +0000

    v2.910 enable the recent and most used plugins

--- v3d_main/v3d/v3d_version_info.cpp
+++ v3d_main/v3d/v3d_version_info.cpp
@@ -50,7 +50,7 @@ Peng, H, Ruan, Z., Atasoy, D., and Sternson, S. (2010) “Automatic reconstructi
 
 namespace v3d {
     // Set current version here.
-    VersionInfo thisVersionOfV3D(2.907);
+    VersionInfo thisVersionOfV3D(2.910);
 
     QString versionnumber = "Vaa3D (3D Visualization-Assisted Analysis) (" +
         thisVersionOfV3D.toQString() +
