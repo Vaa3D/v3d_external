b2bf85f2d09a47538f4017c6ed40d91b0b2724b5
Hanchuan Peng <hanchuan.peng@gmail.com>
2015-02-08 05:45:24 +0000

    tentative version v2.926 disable the color channel table color blending and it seems the problem reported by Hanbo on mismatching z section number is gone. Thus the z-section mismatched display should be in the color channel blending part.

--- v3d_main/v3d/v3d_version_info.cpp
+++ v3d_main/v3d/v3d_version_info.cpp
@@ -50,7 +50,7 @@ Peng, H, Ruan, Z., Atasoy, D., and Sternson, S. (2010) “Automatic reconstructi
 
 namespace v3d {
     // Set current version here.
-    VersionInfo thisVersionOfV3D(2.925);
+    VersionInfo thisVersionOfV3D(2.926);
 
     QString versionnumber = "Vaa3D (3D Visualization-Assisted Analysis) (" +
         thisVersionOfV3D.toQString() +
