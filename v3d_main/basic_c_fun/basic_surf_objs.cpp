ef9710f9646f11b6a42aec77e26cdb9447314926
Hanbo Chen <cojoc.chen@gmail.com>
2015-06-29 23:47:36 +0000

    Fixed the bug of reading empty lines in swc file as NeuronSWC in function readSWC_file().

--- v3d_main/basic_c_fun/basic_surf_objs.cpp
+++ v3d_main/basic_c_fun/basic_surf_objs.cpp
@@ -327,7 +327,7 @@ NeuronTree readSWC_file(const QString& filename)
         count++;
         NeuronSWC S;
 
-        QStringList qsl = QString(buf).split(" ",QString::SkipEmptyParts);
+        QStringList qsl = QString(buf).trimmed().split(" ",QString::SkipEmptyParts);
         if (qsl.size()==0)   continue;
 
         for (int i=0; i<qsl.size(); i++)
