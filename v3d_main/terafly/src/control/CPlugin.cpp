18ba2795d84592c8f2d0f6db7ba0a081bc2df730
Alessandro Bria <a.bria@unicas.it>
2016-04-20 22:20:36 +0200

    fixed bug which caused TIFF plugin error when TeraFly-ing Vaa3D raw color images.

--- v3d_main/terafly/src/control/CPlugin.cpp
+++ v3d_main/terafly/src/control/CPlugin.cpp
@@ -47,7 +47,7 @@ namespace terafly
     *    PARAMETERS    *
     ********************
     ---------------------------------------------------------------------------------------------------------------------------*/
-    std::string version = "2.1.58";         //software version
+    std::string version = "2.1.59";         //software version
     int DEBUG = LEV_MAX;                    //debug level
     debug_output DEBUG_DEST = TO_STDOUT;    // where debug messages should be print (default: stdout)
     std::string DEBUG_FILE_PATH = "/Users/Administrator/Desktop/terafly_debug.log";   //filepath where to save debug information
