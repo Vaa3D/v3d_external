88dc760bb058219d51e09be3b26930a18123ed1c
Hsien-Chi Kuo <hsienchik@alleninstitute.org>
2016-11-28 15:15:27 -0800

    add VersionControl.h

---
+++ v3d_main/v3d/VersionControl.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/*********************************************************
+
+This header file is for switching different code base.
+Only enable line f if under VS2015-Qt5 environment.
+
+Different windows OS versions are still being tested: Win7/Kits_14393, Win10/Kits_14393
+
+MK 11/23/2016
+********************************************************/
+
+//#define USE_Qt5_VS2015_Win7_14393
