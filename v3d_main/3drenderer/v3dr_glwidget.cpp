51cbf4729eb43b05f8193a2aad5f3adc8b57b8b8
Zhi Zhou <alan.zhizhou@gmail.com>
2017-06-06 10:19:29 -0700

    version 3.234: fix a bug when pressing "L" on surface display only 3D view window, Vaa3D will crash

--- v3d_main/3drenderer/v3dr_glwidget.cpp
+++ v3d_main/3drenderer/v3dr_glwidget.cpp
@@ -2597,7 +2597,7 @@ void V3dR_GLWidget::toggleNStrokeCurveDrawing()
 // For curveline detection , by PHC 20170531
 void V3dR_GLWidget::callCurveLineDetector(int option)
 {
-    if (renderer && v3dr_getImage4d(_idep))
+    if (renderer && v3dr_getImage4d(_idep)->get_xy_view())
     {
         if (option==0)
             v3dr_getImage4d(_idep)->get_xy_view()->popupImageProcessingDialog(QString(" -- GD Curveline"));
