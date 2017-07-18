9a953884491bb9c22cc696580ad254edd10520fe
Hanchuan Peng <hanchuan.peng@gmail.com>
2017-07-18 12:37:36 -0700

    v3.316 update some menu text

--- v3d_main/neuron_annotator/gui/NaMainWindow.cpp
+++ v3d_main/neuron_annotator/gui/NaMainWindow.cpp
@@ -2054,7 +2054,7 @@ bool NaMainWindow::createNewDataFlowModel()
 
 
 void NaMainWindow::setTitle(QString title) {
-    setWindowTitle(QString("%1 - V3D Neuron Annotator").arg(title));
+    setWindowTitle(QString("%1 - Vaa3D Neuron Annotator").arg(title));
 }
 
 /* slot */
