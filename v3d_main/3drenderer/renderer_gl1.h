e415bb7ce909293eb1860488d64470c07f288cc6
MKRangers <hsienchik@alleninstitute.org>
2018-05-29 10:08:04 -0700

    roll back to C++98 expression for map::insert

--- v3d_main/3drenderer/renderer_gl1.h
+++ v3d_main/3drenderer/renderer_gl1.h
@@ -479,7 +479,7 @@ public:
 
 	 // --------- loop safe guard for both 3D view and terafly editing mode, MK 2018 May ---------
 	 bool isTera;
-	 map<size_t, size_t> branchSegIDmap;
+	 map<int, double> branchSegIDmap;
 	 int loopCheck(vector<V_NeuronSWC>* curImgSegsPtr, vector<segInfoUnit>* involvedSegsInfoPtr);
 	 void hierarchyReprofile(My4DImage* curImg, long mainSegID, long branchSegID);
 	 void rc_downstreamRelabel(My4DImage* curImg, size_t curStemSegID);
--- v3d_main/3drenderer/renderer_hit.cpp
+++ v3d_main/3drenderer/renderer_hit.cpp
@@ -1594,7 +1594,7 @@ int Renderer_gl1::processHit(int namelen, int names[], int cx, int cy, bool b_me
 				curImg->tracedNeuron = copyToEditableNeuron(p_tree);
 				for (vector<V_NeuronSWC>::iterator segIt = curImg->tracedNeuron.seg.begin(); segIt != curImg->tracedNeuron.seg.end(); ++segIt)
 				{
-					this->branchSegIDmap.insert({ segIt->branchingProfile.ID, segIt->row.begin()->seg_id });
+					this->branchSegIDmap.insert( std::pair<int, double>(segIt->branchingProfile.ID, segIt->row.begin()->seg_id) );
 					cout << segIt->branchingProfile.ID << " " << this->branchSegIDmap[segIt->branchingProfile.ID] << "|";
 				}
 				cout << endl;
