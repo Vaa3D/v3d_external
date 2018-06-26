755a09118c6f5047025e76bdec01a9d28325ffed
shengdianjiang <shengdianjiang@gmail.com>
2018-06-26 21:08:01 +0800

    version3.540:mini map double click to locate highest resolution position

--- v3d_main/v3d/v3d_version_info.cpp
+++ v3d_main/v3d/v3d_version_info.cpp
@@ -55,7 +55,7 @@ Peng, H, Ruan, Z., Atasoy, D., and Sternson, S. (2010) “Automatic reconstructi
 
 namespace v3d {
     // Set current version here.
-    VersionInfo thisVersionOfV3D("3.539a");
+    VersionInfo thisVersionOfV3D("3.540");
 
     QString versionnumber = "Vaa3D (3D Visualization-Assisted Analysis) (" +
         thisVersionOfV3D.toQString() +
