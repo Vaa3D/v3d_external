69d10d272d38d4517703d0a598d79319132685fb
gnayuy <yangy@ibs-yangy-ux1.corp.alleninstitute.org>
2018-07-10 17:10:45 -0700

    update to 3.551c for sync color of marker from om to renderer

--- v3d_main/v3d/v3d_version_info.cpp
+++ v3d_main/v3d/v3d_version_info.cpp
@@ -57,7 +57,7 @@ namespace v3d {
     // Set current version here.
 
 
-    VersionInfo thisVersionOfV3D("3.551b");
+    VersionInfo thisVersionOfV3D("3.551c");
 
     QString versionnumber = "Vaa3D (3D Visualization-Assisted Analysis) (" +
         thisVersionOfV3D.toQString() +
