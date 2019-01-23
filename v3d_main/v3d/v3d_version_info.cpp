f92b4f461f73208e56445772ad4222daedf0d041
Zhi Zhou <alan.zhizhou@gmail.com>
2019-01-22 21:39:15 -0800

    v3.592: add volumn path information in "volumn's info" tab

--- v3d_main/v3d/v3d_version_info.cpp
+++ v3d_main/v3d/v3d_version_info.cpp
@@ -56,7 +56,7 @@ Peng, H, Ruan, Z., Atasoy, D., and Sternson, S. (2010) “Automatic reconstructi
 namespace v3d {
     // Set current version here.
 
-    VersionInfo thisVersionOfV3D("3.591a");
+    VersionInfo thisVersionOfV3D("3.592");
 
     QString versionnumber = "Vaa3D (3D Visualization-Assisted Analysis) (" +
         thisVersionOfV3D.toQString() +
