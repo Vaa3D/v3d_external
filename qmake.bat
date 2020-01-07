32f63bef536eee04adc86f6a75cf97534d270adc
ruanzongcai <ruanzongcai@qq.com>
2020-01-07 11:46:02 +0800

    change qmake.bat nmake_release.bat from vs2010qt474 to vs2013qt486

--- qmake.bat
+++ qmake.bat
@@ -1,3 +1,7 @@
-set QT_BIN=C:\Qt\vs2010x64-qt-4.7.4\bin
+:: a warpper for qmake to switch QT version 
+::
+
+::set QT_BIN=C:\Qt\vs2010x64-qt-4.7.4\bin
+set QT_BIN=C:\Qt\vs2013x64-qt-4.8.6\bin
 
 %QT_BIN%\qmake.exe %*
