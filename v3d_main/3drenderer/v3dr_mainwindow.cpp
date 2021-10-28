62d71bef316c25dc445e8ce24672eae87238965e
Heraldlc <932172851@qq.com>
2021-10-28 15:39:54 +0800

    Update v3dr_mainwindow.cpp

--- v3d_main/3drenderer/v3dr_mainwindow.cpp
+++ v3d_main/3drenderer/v3dr_mainwindow.cpp
@@ -666,10 +666,9 @@ void V3dR_MainWindow::dropEvent(QDropEvent *event)
         qDebug() <<tr("  drop Text data: ")+(mimeData->text().trimmed());
         QString url = mimeData->text().trimmed();
 
-#ifdef Q_OS_LINUX
-        url.remove(0,7); // remove the first 'file://' of the name string, 090125
+        url.remove(0,8); // remove the first 'file://' of the name string, 090125
         url.replace("%20"," ");//fixed the space path issue in 3D viewer on Linux machine by Zhi Zhou May 15 2015
-#endif
+
         qDebug("the file to open=[%s]",qPrintable(url));
         if (glWidget) glWidget->loadObjectFromFile(url);
     }
