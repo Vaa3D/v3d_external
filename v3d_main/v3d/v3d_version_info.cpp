aa6d9fda702e6d27e4bc74b7898cda91a22fe3fe
Heraldlc <932172851@qq.com>
2021-12-14 15:18:46 +0800

    update V6.005

--- v3d_main/v3d/v3d_version_info.cpp
+++ v3d_main/v3d/v3d_version_info.cpp
@@ -56,7 +56,7 @@ Peng, H, Ruan, Z., Atasoy, D., and Sternson, S. (2010) “Automatic reconstructi
 namespace v3d {
     // Set current version here.
 
-    VersionInfo thisVersionOfV3D("6.004");
+    VersionInfo thisVersionOfV3D("6.005");
 
     QString versionnumber = "Vaa3D (3D Visualization-Assisted Analysis) (" +
         thisVersionOfV3D.toQString() +
