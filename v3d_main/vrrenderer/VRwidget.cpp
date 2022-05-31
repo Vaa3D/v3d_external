eacdd9097c8802dbb1bd1f036c1abd8dcb9782e8
justaswell <csznbhh@163.com>
2022-05-31 15:13:17 +0800

    VRwidget

--- v3d_main/vrrenderer/VRwidget.cpp
+++ v3d_main/vrrenderer/VRwidget.cpp
@@ -3,6 +3,42 @@
 
 VRwidget::VRwidget()
 {
+//    leftdata=0;
+//    rightdata=0;
+    drawlayout();
+
+}
+
+VRwidget::~VRwidget()
+{
+    if(isvrclosed==true)
+        qDebug()<<"the vrwidget is closed.";
+//    if(leftdata){
+//        delete leftdata;
+//        leftdata=0;
+//    }
+//    if(rightdata){
+//        delete rightdata;
+//        rightdata=0;
+//    }
+    //qDebug()<<"csz debug VRwidget is destoryed.";
+}
+#include <QCoreApplication>
+void VRwidget::seteye(QImage *texture, int eye)
+{
+    //qDebug()<<texture<<" has been loaded, the eye is "<<eye;
+    if(eye==1){
+        leftmp=leftmp.fromImage(*texture);
+        this->leftlabel->setPixmap(leftmp);
+    }else if(eye==2){
+        rightmp=rightmp.fromImage(*texture);
+        this->rightlabel->setPixmap(rightmp);
+    }
+    QCoreApplication::processEvents();
+}
+
+void VRwidget::drawlayout()
+{
     this->setWindowTitle("VRviewer");
     this->setFixedSize(1920,1080);
     leftlabel=new QLabel(this);
@@ -17,12 +53,8 @@ VRwidget::VRwidget()
     layout->addWidget(leftlabel);
     layout->addWidget(rightlabel);
     this->setLayout(layout);
-}
-
-VRwidget::~VRwidget()
-{
-    if(isvrclosed==true)
-        qDebug()<<"the vrwidget is closed.";
+    isvrclosed=false;
+    isdrawlayout=true;
 }
 
 #include <QCloseEvent>
